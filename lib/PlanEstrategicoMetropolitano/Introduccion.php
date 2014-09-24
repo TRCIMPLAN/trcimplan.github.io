6f4258d8c3ea6f0224f4bb3d3bfb9304516587d3
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2014-09-24 13:08:48 -0500

    Modifiqué la introducción al PEM.

--- lib/PlanEstrategicoMetropolitano/Introduccion.php
+++ lib/PlanEstrategicoMetropolitano/Introduccion.php
@@ -33,7 +33,7 @@ class Introduccion extends \Base\Publicacion {
     public function __construct() {
         // Título, autor y fecha con el formato AAAA-MM-DD
         $this->nombre        = 'Plan Estratégico Metropolitano';
-        $this->autor         = 'Autor';
+        $this->autor         = 'TrcIMPLAN';
         $this->fecha         = '2014-09-24';
         // El nombre del archivo a crear (obligatorio), la ruta a la imagen previa y el encabezado (opcionales). Use minúsculas, números y/o guiones medios.
         $this->archivo       = 'introduccion';
@@ -51,19 +51,19 @@ class Introduccion extends \Base\Publicacion {
         $this->contenido     = <<<FINAL
 <h2>¿Qué es el Plan Estratégico Metropolitano?</h2>
 
-<p>El Plan Estratégico Metropolitano es un esfuerzo de planeación participativa que encabeza el Consejo Directivo del IMPLAN Torreón, para atender la necesidad urgente de elevar el nivel de competitividad para el desarrollo económico y social de la Zona Metropolitana de La Laguna.</p>
+<p>El <strong>Plan Estratégico Metropolitano (PEM)</strong> es un esfuerzo de planeación participativa que encabeza el Consejo Directivo del IMPLAN Torreón, para <strong>atender la necesidad urgente de elevar el nivel de competitividad</strong> para el desarrollo económico y social de la <strong>Zona Metropolitana de La Laguna</strong>.</p>
 
 <h2>¿En qué me beneficia?</h2>
 
-<p>Es un amplo ejercicio de corresponsabilidad y gobernanza que concentrará las visiones y opiniones de seis grandes temas: Buen Gobierno, Desarrollo Económico y Competitividad, Desarrollo Social, Movilidad y Transporte, Sustentabilidad y Entorno Urbano.</p>
+<p>El PEM es un amplo ejercicio de corresponsabilidad y gobernanza que concentrará las visiones y opiniones de seis grandes temas: <strong>Buen Gobierno, Desarrollo Económico y Competitividad, Desarrollo Social, Movilidad y Transporte, Sustentabilidad y Entorno Urbano.</strong></p>
 
-<p>Tu participación será vital para identificar y concretar las líneas de acción que seguirán gobernantes y gobernados, del amplio número de necesidades y proyectos que requiere la Laguna para ser mejor.</p>
+<p>Tu participación será vital para <strong>identificar y concretar las líneas de acción más efectivas</strong> que seguirán <strong>gobernantes y gobernados</strong>, de la gran cantidad de necesidades y proyectos que requiere la Laguna para ser mejor.</p>
 
 <h2>¿Quienes lo elaboran?</h2>
 
-<p>Es formulado por sobresalientes y diversos actores locales, motivados por su compromiso de mejorar la zona metropolitana. Entre ellos están representantes de instituciones gubernamentales y no gubernamentales, grupos de profesionales expertos y actores sociales estratégicos.</p>
+<p>El PEM es realizado por <strong>sobresalientes y diversos actores locales</strong>, motivados por su compromiso de mejorar la zona metropolitana. Entre ellos están representantes de instituciones gubernamentales y no gubernamentales, grupos de profesionales expertos y personajes sociales estratégicos.</p>
 
-<p>Se apoyarán con información proporcionada por ciudadanos y el equipo técnico del IMPLAN Torreón quienes coordinarán, ordenarán y publicarán este procedimiento.</p>
+<p>Se apoyarán con las propuestas proporcionadas por los <strong>ciudadanos</strong> y con el equipo técnico del IMPLAN Torreón, quienes coordinarán, ordenarán y publicarán este proceso.</p>
 
 <h2>¿De qué forma puedo participar?</h2>
 
