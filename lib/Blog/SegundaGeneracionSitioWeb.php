a30090481fbba90e7295a337b38c5af1bec14555
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-01-20 13:43:56 -0600

    Hice visible el análisis Segunda Generación Sitio Web.

--- lib/Blog/SegundaGeneracionSitioWeb.php
+++ lib/Blog/SegundaGeneracionSitioWeb.php
@@ -33,7 +33,7 @@ class SegundaGeneracionSitioWeb extends \Base\Publicacion {
     public function __construct() {
         // Título, autor y fecha con el formato AAAA-MM-DD
         $this->nombre           = 'Segunda generación del sitio web';
-        $this->autor            = 'Ing. Guillermo Valdés, Lic. Israel S. Charur, Lic. Hugo Ramírez';
+        $this->autor            = 'Ing. Guillermo Valdés';
         $this->fecha            = '2015-01-20';
         // El nombre del archivo a crear (obligatorio), la ruta a la imagen previa y el encabezado (opcionales). Use minúsculas, números y/o guiones medios.
         $this->archivo          = 'segunda-generacion-sitio-web';
@@ -41,14 +41,14 @@ class SegundaGeneracionSitioWeb extends \Base\Publicacion {
      // $this->encabezado       = '<img class="img-responsive encabezado-imagen" src="segunda-generacion-sitio-web/encabezado.jpg">';
      // $this->encabezado_color = '#646464';
         // La descripción y claves dan información a los buscadores y redes sociales. Las categorías son de uso interno.
-        $this->descripcion      = 'A partir de enero de 2015 se puso en línea el nuevo sitio web del IMPLAN Torreón.';
+        $this->descripcion      = 'A partir de enero de 2015 se puso en línea el nuevo sitio web del IMPLAN Torreón. Se integraron todos los servicios y se adapta a teléfonos celulares y tablets.';
         $this->claves           = 'IMPLAN, Torreon';
-        $this->categorias       = array('SMI');
+        $this->categorias       = array('SMI', 'SIG');
         // NO CAMBIE el nombre_menu y el directorio. Están definidos para Análisis Publicados.
         $this->directorio       = 'blog';
         $this->nombre_menu      = 'Análisis Publicados';
         // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
-        $this->estado           = 'revisar';
+        $this->estado           = 'publicar';
         // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
      // $this->para_compartir   = true;
         // El contenido HTML y el JavaScript
