2b68ef25c33983e9035525f879eba493da38173d
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-01-28 15:45:48 -0600

    Corregido y aumentado artículo Prevención Delito Espacio Publico.

---
+++ lib/Base/Carrusel.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * SMIbeta - Carrusel
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Namespace
+namespace Base;
+
+/**
+ * Clase Carrusel
+ */
+class Carrusel {
+
+    protected $id;
+    protected $diapositivas;
+    protected $contador = 0;
+
+    /**
+     * Constructor
+     *
+     * @param string ID cadena de texto que sera el ID del tag DIV
+     */
+    public function __construct($id) {
+        $this->id = $id;
+    } // constructor
+
+    /**
+     * Agregar Diapositiv
+     *
+     * @param string URL con la ruta a la imagen
+     * @param string Texto alterno para la imagen
+     * @param string Descripción opcional
+     */
+    public function agregar_diapositiva($url, $alt='', $descripcion='') {
+        $this->diapositivas[] = array(
+            'url'         => $url,
+            'alt'         => $alt,
+            'descripcion' => $descripcion);
+        $this->contador++;
+    } // agregar_diapositiva
+
+    /**
+     * HTML
+     *
+     * @return string Código HTML
+     */
+    public function html() {
+        // Validar que haya diapositivas
+        if ($this->contador == 0) {
+            throw new \Exception('Error en Carrusel: No hay diapositivas.');
+        }
+        // Acumular
+        $indicators = array();
+        $c          = 0;
+        foreach ($this->diapositivas as $d) {
+            if ($c == 0) {
+                $indicators[] = "    <li data-target=\"#carousel-example-generic\" data-slide-to=\"$c\" class=\"active\"></li>";
+            } else {
+                $indicators[] = "    <li data-target=\"#carousel-example-generic\" data-slide-to=\"$c\"></li>";
+            }
+            $c++;
+        }
+        $carusel_indicators = implode("\n", $indicators);
+        // Acumular
+        $slides = array();
+        $c      = 0;
+        foreach ($this->diapositivas as $d) {
+            if ($c == 0) {
+                if ($d['descripcion'] == '') {
+                    $slides[] = <<<FINAL
+    <div class="item active">
+      <img src="{$d['url']}" alt="{$d['alt']}">
+    </div>
+FINAL;
+                } else {
+                    $slides[] = <<<FINAL
+    <div class="item active">
+      <img src="{$d['url']}" alt="{$d['alt']}">
+      <div class="carousel-caption">
+        <p>{$d['descripcion']}</p>
+      </div>
+    </div>
+FINAL;
+                }
+            } else {
+                if ($d['descripcion'] == '') {
+                    $slides[] = <<<FINAL
+    <div class="item">
+      <img src="{$d['url']}" alt="{$d['alt']}">
+    </div>
+FINAL;
+                } else {
+                    $slides[] = <<<FINAL
+    <div class="item">
+      <img src="{$d['url']}" alt="{$d['alt']}">
+      <div class="carousel-caption">
+        <p>{$d['descripcion']}</p>
+      </div>
+    </div>
+FINAL;
+                }
+            }
+            $c++;
+        }
+        $carusel_slides = implode("\n", $slides);
+        // Entregar
+        return <<<FINAL
+<div id="{$this->id}" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+$carusel_indicators
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+$carusel_slides
+  </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+FINAL;
+    } // html
+
+} // Clase Carrusel
+
+?>
--- lib/Blog/PrevencionDelitoEspacioPublico.php
+++ lib/Blog/PrevencionDelitoEspacioPublico.php
@@ -32,198 +32,34 @@ class PrevencionDelitoEspacioPublico extends \Base\Publicacion {
      */
     public function __construct() {
         //
+        $carrusel1 = new \Base\Carrusel('carrusel-1');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-fachas-casa.jpg',           '', 'El espacio urbano se caracteriza por nuestras fachas de casa y negocios, estas a su vez pueden incentivar la percepción de inseguridad de quien usa la calle.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-muro.jpg',                  '', 'El muro es el símbolo más visible del miedo.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-control-social.jpg',        '', 'Es importante diseñar espacios que promuevan el control social, una estrategias es a traves de diseños que eviten las barreras visuales.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-no-alenta-uso.jpg',         '', 'Este espacio no alenta el uso de la comunidad que lo rodea.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-miedo.jpg',                 '', 'La ciudad que es construida por el miedo.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-disposicion-parques.jpg',   '', 'La disposición de parques en el espacio no garantiza la apropiación de este por la población.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-diversificacion-suelo.jpg', '', 'La diversificación de usos de suelo, entre otras cosas, tiene la capacidad de promover el uso de los espacios durante más horas al día.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-mallas.jpg',                '', 'La mallas para delimitar el espacio público también es un sinónimos de miedo. Esta estrategia no a demostrado ser valiosa para que el habitante se sienta seguro.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-marcas-negativas.jpg',      '', 'Las marcas negativas dentro del espacio fomentan la percepción de inseguridad.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-dignifiquen.jpg',           '', 'Se requieren espacios públicos que dignificen las comunidades.');
+        $carrusel1->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-1-ciudad-limpia.jpg',         '', 'Una ciudad limpia es un factor que apoya a disminuir la percepción de inseguridad. Esto es responsabilidad de todos los que generamos basura.');
         //
-        //
-        $carrusel1 = <<<FINAL
-<div id="carousel-1" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="8"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="9"></li>
-  </ol>
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="prevencion-delito-espacio-publico/01-muro-ampliacion-senderos-800.jpg" alt="Ampliacion Senderos Torreon">
-      <div class="carousel-caption">
-        <p>El muro es el símbolo más visible del miedo. Ampliación Senderos, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/02-tajito-torreon-800.jpg" alt="El tajito Torreon">
-      <div class="carousel-caption">
-        <p>Este espacio no alenta el uso de la comunidad que lo rodea. El tajito, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/03-disposicion-parques-800.jpg" alt="Ampliacion Senderos Torreon">
-      <div class="carousel-caption">
-        <p>La disposición de parques en el espacio no garantiza la apropiación de este por la población. Ampliación Senderos, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/04-participacion-ciudadana-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>La participación ciudadana tiene la capacidad de construir compromisos. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/05-participacion-grupos-vulnerables-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>La participación de los grupos vulnerables es de vital importancia. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/06-condominio-anhattan-800.jpg" alt="Condominio Los Manhattan">
-      <div class="carousel-caption">
-        <p>Las marcas negativas dentro del espacio fomentan la percepción de inseguridad. Condominio Los Manhattan, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/07-percepcion-seguridad-inseguridad-800.jpg" alt="Espacios urbanos">
-      <div class="carousel-caption">
-        <p>Los espacios urbanos pueden detonar percepción de seguridad o inseguridad.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/08-diferentes-aspiraciones-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>Ninguna comunidad es igual a otra y cada una tiene en conjunto diferentes aspiraciones. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/09-dignificen-comunidades-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>Se requieren espacios públicos que dignificen las comunidades. Zaragoza Sur, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/10-taller-participacion-ciudadana-800.jpg" alt="Taller de participación ciudadana">
-      <div class="carousel-caption">
-        <p>Taller de participación ciudadana lidereado por iniciativa de planeación en comunidades TOGETHER NORTH JERSEY en Nueva Jersey, EUA.</p>
-      </div>
-    </div>
-  </div>
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-FINAL;
-        //
-        //
-        //
-        $carrusel2 = <<<FINAL
-<div id="carousel-2" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="8"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="9"></li>
-  </ol>
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="prevencion-delito-espacio-publico/01-muro-ampliacion-senderos-800.jpg" alt="Ampliacion Senderos Torreon">
-      <div class="carousel-caption">
-        <p>El muro es el símbolo más visible del miedo. Ampliación Senderos, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/02-tajito-torreon-800.jpg" alt="El tajito Torreon">
-      <div class="carousel-caption">
-        <p>Este espacio no alenta el uso de la comunidad que lo rodea. El tajito, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/03-disposicion-parques-800.jpg" alt="Ampliacion Senderos Torreon">
-      <div class="carousel-caption">
-        <p>La disposición de parques en el espacio no garantiza la apropiación de este por la población. Ampliación Senderos, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/04-participacion-ciudadana-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>La participación ciudadana tiene la capacidad de construir compromisos. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/05-participacion-grupos-vulnerables-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>La participación de los grupos vulnerables es de vital importancia. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/06-condominio-anhattan-800.jpg" alt="Condominio Los Manhattan">
-      <div class="carousel-caption">
-        <p>Las marcas negativas dentro del espacio fomentan la percepción de inseguridad. Condominio Los Manhattan, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/07-percepcion-seguridad-inseguridad-800.jpg" alt="Espacios urbanos">
-      <div class="carousel-caption">
-        <p>Los espacios urbanos pueden detonar percepción de seguridad o inseguridad.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/08-diferentes-aspiraciones-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>Ninguna comunidad es igual a otra y cada una tiene en conjunto diferentes aspiraciones. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/09-dignificen-comunidades-800.jpg" alt="Zaragoza Sur">
-      <div class="carousel-caption">
-        <p>Se requieren espacios públicos que dignificen las comunidades. Zaragoza Sur, Torreón.</p>
-      </div>
-    </div>
-    <div class="item">
-      <img src="prevencion-delito-espacio-publico/10-taller-participacion-ciudadana-800.jpg" alt="Taller de participación ciudadana">
-      <div class="carousel-caption">
-        <p>Taller de participación ciudadana lidereado por iniciativa de planeación en comunidades TOGETHER NORTH JERSEY en Nueva Jersey, EUA.</p>
-      </div>
-    </div>
-  </div>
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-FINAL;
-        //
-        //
-        //
+        $carrusel2 = new \Base\Carrusel('carrusel-2');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-construccion-calles.jpg',    '', 'El ciudadano tiene la capacidad de incidir en la construcción de las calles y parques de manera positiva o negativa, también puede construir socialmente su seguridad.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-control-aprecio.jpg',        '', 'El mantenimiento del espacio público habla de que los habitantes tienen control y aprecio sobre de él.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-construccion-seguridad.jpg', '', 'El mantenimiento y limpieza del espacio público es factor importante en la construcción de seguridad.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-apropiacion-sociedad.jpg',   '', 'Es importante la apropiación de la sociedad de los proyectos, promovera su uso y cuidado en el tiempo por parte de la comunidad.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-incluir-comunidad.jpg',      '', 'Incluir a la comunidad permite identifar aquellos espacios en los cuales ellos perciben inseguros.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-calidad-vida.jpg',           '', 'La calidad de vida debe encontrarse fuera de las murallas, en lo común a todos, palpable en el espacio público.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-grupos-vulnerables.jpg',     '', 'La participación de los grupos vulnerables es de vital importancia.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-ver-ser-visto.jpg',          '', 'La posibilidad de ver y ser visto, donde existe cohesición social, desminuye la percepción de inseguridad.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-ninguna-igual-otra.jpg',     '', 'Ninguna comunidad es igual a otra y cada una tiene en conjunto diferentes aspiraciones. Zaragoza Sur, Torreón, 2009 Tesis universitaria.');
+        $carrusel2->agregar_diapositiva('prevencion-delito-espacio-publico/carrusel-2-taller-participacion.jpg',   '', 'Taller de participación ciudadana lidereado por iniciativa de planeación en comunidades TOGETHER NORTH JERSEY en Nueva Jersey, EUA.');
         // Título, autor y fecha con el formato AAAA-MM-DD
         $this->nombre           = 'Prevención del delito mediante la construcción del espacio público';
         $this->autor            = 'Arq. Victoria Aranzábal';
-        $this->fecha            = '2015-01-22';
+        $this->fecha            = '2015-01-28';
         // El nombre del archivo a crear (obligatorio), la ruta a la imagen previa y el encabezado (opcionales). Use minúsculas, números y/o guiones medios.
         $this->archivo          = 'prevencion-delito-espacio-publico';
         $this->imagen_previa    = 'prevencion-delito-espacio-publico/imagen-previa.jpg';
@@ -237,15 +73,15 @@ FINAL;
         $this->directorio       = 'blog';
         $this->nombre_menu      = 'Análisis Publicados';
         // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
-        $this->estado           = 'revisar';
+        $this->estado           = 'publicar';
         // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
      // $this->para_compartir   = true;
         // El contenido HTML y el JavaScript
         $this->contenido        = implode("\n", array(
             $this->cargar_archivo_markdown_extra('lib/Blog/PrevencionDelitoEspacioPublicoParte1.md'),
-            $carrusel1,
+            $carrusel1->html(),
             $this->cargar_archivo_markdown_extra('lib/Blog/PrevencionDelitoEspacioPublicoParte2.md'),
-            $carrusel2,
+            $carrusel2->html(),
             $this->cargar_archivo_markdown_extra('lib/Blog/PrevencionDelitoEspacioPublicoParte3.md')));
         $this->javascript       = '';
     } // constructor
