6ec7a6ba6e1c084e8c33be28a2564ad151cf2b7b
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-01-28 11:12:27 -0600

    Actualización artículo Luly.

--- lib/Blog/PrevencionDelitoEspacioPublico.php
+++ lib/Blog/PrevencionDelitoEspacioPublico.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * SMIbeta - Prevención del delito en el espacio público
+ * SMIbeta - Prevención del delito mediante la construcción del espacio público
  *
  * Copyright (C) 2015 Guillermo Valdés Lozano
  *
@@ -34,8 +34,101 @@ class PrevencionDelitoEspacioPublico extends \Base\Publicacion {
         //
         //
         //
-        $carrusel = <<<FINAL
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        $carrusel1 = <<<FINAL
+<div id="carousel-1" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="8"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="9"></li>
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="prevencion-delito-espacio-publico/01-muro-ampliacion-senderos-800.jpg" alt="Ampliacion Senderos Torreon">
+      <div class="carousel-caption">
+        <p>El muro es el símbolo más visible del miedo. Ampliación Senderos, Torreón.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/02-tajito-torreon-800.jpg" alt="El tajito Torreon">
+      <div class="carousel-caption">
+        <p>Este espacio no alenta el uso de la comunidad que lo rodea. El tajito, Torreón.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/03-disposicion-parques-800.jpg" alt="Ampliacion Senderos Torreon">
+      <div class="carousel-caption">
+        <p>La disposición de parques en el espacio no garantiza la apropiación de este por la población. Ampliación Senderos, Torreón.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/04-participacion-ciudadana-800.jpg" alt="Zaragoza Sur">
+      <div class="carousel-caption">
+        <p>La participación ciudadana tiene la capacidad de construir compromisos. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/05-participacion-grupos-vulnerables-800.jpg" alt="Zaragoza Sur">
+      <div class="carousel-caption">
+        <p>La participación de los grupos vulnerables es de vital importancia. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/06-condominio-anhattan-800.jpg" alt="Condominio Los Manhattan">
+      <div class="carousel-caption">
+        <p>Las marcas negativas dentro del espacio fomentan la percepción de inseguridad. Condominio Los Manhattan, Torreón.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/07-percepcion-seguridad-inseguridad-800.jpg" alt="Espacios urbanos">
+      <div class="carousel-caption">
+        <p>Los espacios urbanos pueden detonar percepción de seguridad o inseguridad.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/08-diferentes-aspiraciones-800.jpg" alt="Zaragoza Sur">
+      <div class="carousel-caption">
+        <p>Ninguna comunidad es igual a otra y cada una tiene en conjunto diferentes aspiraciones. Zaragoza Sur, Torreón, 2009 Tesis universitaria.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/09-dignificen-comunidades-800.jpg" alt="Zaragoza Sur">
+      <div class="carousel-caption">
+        <p>Se requieren espacios públicos que dignificen las comunidades. Zaragoza Sur, Torreón.</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="prevencion-delito-espacio-publico/10-taller-participacion-ciudadana-800.jpg" alt="Taller de participación ciudadana">
+      <div class="carousel-caption">
+        <p>Taller de participación ciudadana lidereado por iniciativa de planeación en comunidades TOGETHER NORTH JERSEY en Nueva Jersey, EUA.</p>
+      </div>
+    </div>
+  </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+FINAL;
+        //
+        //
+        //
+        $carrusel2 = <<<FINAL
+<div id="carousel-2" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -128,7 +221,7 @@ FINAL;
         //
         //
         // Título, autor y fecha con el formato AAAA-MM-DD
-        $this->nombre           = 'La prevención del delito en el espacio público';
+        $this->nombre           = 'Prevención del delito mediante la construcción del espacio público';
         $this->autor            = 'Arq. Victoria Aranzábal';
         $this->fecha            = '2015-01-22';
         // El nombre del archivo a crear (obligatorio), la ruta a la imagen previa y el encabezado (opcionales). Use minúsculas, números y/o guiones medios.
@@ -150,9 +243,9 @@ FINAL;
         // El contenido HTML y el JavaScript
         $this->contenido        = implode("\n", array(
             $this->cargar_archivo_markdown_extra('lib/Blog/PrevencionDelitoEspacioPublicoParte1.md'),
-            $carrusel,
+            $carrusel1,
             $this->cargar_archivo_markdown_extra('lib/Blog/PrevencionDelitoEspacioPublicoParte2.md'),
-            $carrusel,
+            $carrusel2,
             $this->cargar_archivo_markdown_extra('lib/Blog/PrevencionDelitoEspacioPublicoParte3.md')));
         $this->javascript       = '';
     } // constructor
--- lib/Blog/PrevencionDelitoEspacioPublicoParte3.md
+++ lib/Blog/PrevencionDelitoEspacioPublicoParte3.md
@@ -1,6 +1,15 @@
 
-Los proyectos más exitosos son aquellos que además de intervenir el lugar físicamente, se acompañan de programas sociales, educación y proximidad policial. En Latinoamérica de los casos de éxito y reconocido a nivel internacional se encuentra la ciudad de Medellín, donde las áreas más marginadas han sido intervenidas no sólo con la mejora del espacio público, también con la dignificación de los espacios de educación, sistemas de movilidad,  presencia policial en espacios estratégicos, etc.
+Los proyectos más exitosos son aquellos que además de intervenir el lugar físicamente, se acompañan de programas sociales, educación y proximidad policial. En Latinoamérica se encuentra la ciudad de Medellín, como caso paradigmático de éxito, reconocido a nivel internacional, donde las áreas más marginadas han sido intervenidas no sólo con la mejora del espacio público, también con la dignificación de los espacios de educación, sistemas de movilidad,  presencia policial en espacios estratégicos, etc.
 
 La seguridad no se puede construir de manera limitada con estrategias de control (reforzamiento policial), es imposible poner un policía en cada cuadra, tampoco existen los recursos de manera unilateral para intervenir en cada espacio crítico de la ciudad, de ahí la necesidad de generar proyectos integrales con fuerte colaboración social.
 
-El diseño urbano adecuado y bien diseñado puede ser un vehículo de corto plazo para mejorar la calidad de vida reduciendo la percepción de inseguridad y los delitos de oportunidad; pero no sólo eso, la relevancia del espacio público en el sentido más completo es tal que: de acuerdo a la publicación de ONU- Habitat: “Streets as Public Spaces and Drivers of Urban Prosperity”, existe una correlación directa entre la expansión de las calles, su calidad y uso con la prosperidad económica de las ciudades, además tiene la posibilidad de construir la confianza de la población en las instituciones públicas y contribuir a disminución del deterioro de los procesos democráticos.
+El diseño urbano adecuado puede ser un vehículo de corto plazo para mejorar la calidad de vida reduciendo la percepción de inseguridad y los delitos de oportunidad; pero no sólo eso, la relevancia del espacio público en el sentido más completo es tal que: de acuerdo a la publicación de ONU- Habitat: “Streets as Public Spaces and Drivers of Urban Prosperity”, existe una correlación directa entre la expansión las calles, su calidad y uso con la prosperidad económica de las ciudades, además construye confianza de la población en las instituciones públicas y contribuye a disminuir el deterioro de los procesos democráticos.
+
+### Notas
+
+1. Encuesta de percepción de inseguridad, 6ª. Edición, Tercera entrega, Agosto 2014. http://ccilaguna.org.mx/encuestas-de-percepcion
+2. Encuesta Nacional de Victimización y Percepción sobre Seguridad Pública, INEGI, 2014. http://www.inegi.org.mx/est/contenidos/proyectos/encuestas/hogares/regulares/envipe/
+3. Informe 2013, Corporación Latinobarómetro. http://www.latinobarometro.org/documentos/LATBD_INFORME_LB_2013.pdf
+4. Dentro de estas aportaciones las más relevantes son las siguientes publicaciones: 1. Prevención del crimen a través del diseño ambiental, C. RayJeffery, 1971. La vida y muerte de las grandes ciudades americanas, Jane Jacobs, 1961.
+El espacio defendible, Oscar Newman, 1972.
+5. Encuesta Nacional de Victimización y Percepción sobre Seguridad Pública, INEGI, 2014.
