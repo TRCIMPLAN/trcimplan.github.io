88d727df84b508f922463dca1de34bfb3c23eeb5
TrcIMPLAN Servidor <webmaster@trcimplan.mx>
2015-03-27 13:35:37 -0600

    Corte al 27/mar 13:35.

--- lib/SMIIndicadoresTorreon/SeguridadRoboATranseuntes.php
+++ lib/SMIIndicadoresTorreon/SeguridadRoboATranseuntes.php
@@ -22,8 +22,8 @@ class SeguridadRoboATranseuntes extends \Base\Publicacion {
         $this->directorio   = 'indicadores-torreon';
         $this->archivo      = 'seguridad-robo-a-transeuntes';
         $this->descripcion  = 'Robos a Transeuntes totales por mes.';
-        $this->claves       = 'Torreón';
-        $this->categorias   = array();
+        $this->claves       = 'Torreón, Seguridad, Delincuencia';
+        $this->categorias   = array('Seguridad', 'Delincuencia');
         $this->region_nivel = 101;
         $this->contenido    = <<<FINAL
   <ul class="nav nav-tabs lenguetas" id="smi-indicador">
@@ -211,6 +211,138 @@ class SeguridadRoboATranseuntes extends \Base\Publicacion {
           </tr>
         </thead>
         <tbody>
+          <tr>
+            <td>17/02/2015</td>
+            <td>Análisis</td>
+            <td><a href="../blog/seguridad-humana.html">Seguridad Humana</a></td>
+            <td>La población se siente insegura no solo por las acciones de la delincuencia organizada, también la genera la falta de empleo, la pobreza persistente, la ausencia de acceso a cuidados básicos de salud, la violencia física, la violencia familiar, la represión política, la violación a los derechos humanos, el deterioro ambiental, la incertidumbre política, la falta de educación, la imposibilidad de ejercer el derecho de libre expresión y la ausencia de equidad de género entre otros.</td>
+          </tr>
+          <tr>
+            <td>07/01/2015</td>
+            <td>Análisis</td>
+            <td><a href="../blog/prevencion-delincuencia-juvenil.html">La prevención de la Delincuencia Juvenil, elemento central de la prevención integral del delito y la violencia</a></td>
+            <td>Dentro de una estrategia de prevención integral del delito, la delincuencia juvenil constituye desde luego el elemento fundamental, si lo que se pretende es evitar que niños de doce años decidan formar parte de la delincuencia.</td>
+          </tr>
+          <tr>
+            <td>18/11/2014</td>
+            <td>Análisis</td>
+            <td><a href="../blog/prevencion-delito-seguridad.html">La Prevención del Delito como elemento fundamental para la Seguridad</a></td>
+            <td>La prevención del delito debe ser el elemento central en la búsqueda de la seguridad en la Zona Metropolitana de la Laguna (ZML).</td>
+          </tr>
+          <tr>
+            <td>25/09/2014</td>
+            <td>Análisis</td>
+            <td><a href="../blog/comportamiento-tendencia-incidencia-delictiva.html">Comportamiento y tendencia de la incidencia delictiva. Una visión ampliada.</a></td>
+            <td>Mostramos las tendencias de los delitos más recurrentes en la ciudad de Torreón; con datos del Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública.</td>
+          </tr>
+          <tr>
+            <td>02/06/2014</td>
+            <td>Análisis</td>
+            <td><a href="../blog/zml-sistema-derecho-confiable-objetivo.html">La Zona Metropolitana de La Laguna y sus municipios en Sistema de Derecho Confiable y Objetivo</a></td>
+            <td>De acuerdo al ICU 2012, es el subíndice más débil de la región; algunos de sus subíndices ponderados con alto peso por el IMCO corresponden a rubros de seguridad pública.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-cantidad-de-policias.html">Cantidad de Policías en Torreón</a></td>
+            <td>Cantidad total de policías en el municipio.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-delitos.html">Delitos en Torreón</a></td>
+            <td>Total de delitos cometidos.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/sociedad-estimacion-de-menores-huerfanos-por-agresiones.html">Estimación de Menores Huérfanos por Agresiones en Torreón</a></td>
+            <td>Menores de edad que han perdido a padre o madre a causa de agresión (homicidio).</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-homicidios.html">Homicidios en Torreón</a></td>
+            <td>Número de homicidios por mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-presuntos-delincuentes-del-fuero-comun.html">Presuntos Delincuentes del Fuero Común en Torreón</a></td>
+            <td>Cantidad de presuntos delincuentes registrados en los juzgados de primera instancia del fuero común por año.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-presuntos-delincuentes-del-fuero-federal.html">Presuntos Delincuentes del Fuero Federal en Torreón</a></td>
+            <td>Presuntos delincuentes registrados en los juzgados de primera instancia en materia penal del fuero federal</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-presuntos-delincuentes-por-narcoticos.html">Presuntos Delincuentes por Narcóticos en Torreón</a></td>
+            <td>Presuntos delincuentes por delitos en materia de narcóticos registrados en los juzgados del fuero federal.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-robo-de-vehiculos.html">Robo de Vehículos en Torreón</a></td>
+            <td>Robo de vehículos con violencia y sin violencia cometidos en el mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-secuestros.html">Secuestros en Torreón</a></td>
+            <td>Cantidad de secuestros en el mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-sentenciados-registrados-del-fuero-comun.html">Sentenciados Registrados del Fuero Común en Torreón</a></td>
+            <td>Cantidad de sentenciados registrados en los juzgados de primera instancia del fuero común por año.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-sentenciados-registrados-del-fuero-federal.html">Sentenciados Registrados del Fuero Federal en Torreón</a></td>
+            <td>Sentenciados registrados en los juzgados de primera instancia en materia penal del fuero federal</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-sentenciados-en-materia-de-narcoticos-registrados-en-el-fuero-federal.html">Sentenciados en Materia de Narcóticos Registrados en el Fuero Federal en Torreón</a></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-tasa-de-delitos.html">Tasa de Delitos en Torreón</a></td>
+            <td>Delitos por cada 100 mil habitantes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-tasa-de-homicidios.html">Tasa de Homicidios en Torreón</a></td>
+            <td>Número de homicidios por cada 100 mil habitantes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-tasa-de-policias-por-habitante.html">Tasa de Policías por Habitante en Torreón</a></td>
+            <td>Cantidad de policías por cada mil habitantes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-tasa-de-robo-de-vehiculos.html">Tasa de Robo de VehÍculos en Torreón</a></td>
+            <td>Cantidad de robos de vehículos por cada 100 mil habitantes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-torreon/seguridad-tasa-de-secuestro.html">Tasa de Secuestro en Torreón</a></td>
+            <td>Homicidios por habitantes por 100 mil</td>
+          </tr>
         </tbody>
       </table>
     </div>
