6af4d0d14494495402e067a4e8ac1c6f8b216df8
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-04-22 10:11:39 -0500

    Agregué análisis Necesidad de Diagnóstico sobre Delincuencia Juvenil.

---
+++ lib/Blog/NecesidadDiagnosticoSobreDelincuenciaJuvenil.md
@@ -0,0 +1,20 @@
+
+De acuerdo a su definición, el término Diagnóstico (del griego diagnostikós, a su vez del prefijo día-, “a través”, y gnosis, “conocimiento” o “apto para conocer”) alude, en general, al análisis que se realiza para determinar cualquier situación y cuáles son las tendencias. Esta determinación se realiza sobre la base de datos y hechos recogidos y ordenados sistemáticamente, que permiten juzgar mejor qué es lo que está pasando.
+
+La definición del término es pertinente para dejar claro a que nos referimos cuando hablamos de diagnóstico, sobre todo porque considero que no existe un esfuerzo de este tipo (al menos no se ha hecho público ningún diagnóstico reciente sobre este tema por parte de instituciones públicas o privadas) en nuestro municipio y dadas las actuales circunstancias es un tema digno de un análisis más de fondo.
+
+El contexto de violencia en el que hemos vivido en los últimos cuatro años, si bien es cierto ha disminuido de forma considerable, también es cierto que ha dejado secuelas importantes sobre todo en nuestros jóvenes, es decir, un número considerable de ellos se han involucrado en actividades ilícitas, aunque no todos al narcotráfico.
+
+Es importante tomar en cuenta la situación que revelan algunos periódicos de circulación local o regional sobre el ambiente que rodea desde hace algunos años a un considerable número de jóvenes en nuestra región, valdría la pena desarrollar un trabajo conjunto entre autoridades y organizaciones sociales dedicadas al estudio de estos temas con la finalidad de conocer la situación real y establecer una política pública pertinente.
+
+El ambiente al que nos referimos tiene que ver con la pobreza, la marginación, la falta de oportunidades para su educación y la falta de empleo, elementos que en conjunto ha llevado a un número considerable de jóvenes a tomar la decisión de integrarse a algunas de las variantes de la delincuencia
+
+No podemos generalizar sobre este tipo de decisiones, sin embargo, se percibe alrededor de los jóvenes un conjunto de factores de riesgo que están presentes continuamente en su ámbito de convivencia.
+
+Esto representa una situación de indefensión puesto que bajo estas circunstancias y debido a que toda su vida se han desenvuelto en este ambiente, nuestros jóvenes carecen de recursos no solo económicos sino sobre todo conductuales, cognitivos e intelectuales para hacer frente a estos riesgos.
+
+En este sentido es importante saber a ciencia cierta cuantos jóvenes tenemos en nuestro municipio, cuáles son sus condiciones de vida, cuántos de ellos trabajan, cuantos comparten el estudio con el trabajo, quienes son estudiantes de tiempo completo, cuantos viven en zonas marginadas, advertir también cuántos de ellos son menores infractores y cuántos son considerados delincuentes, y desde luego conocer el tipo de delitos que han cometido.
+
+Para efectos de la implementación de una política pública de prevención que sea eficaz, es necesario tener un conocimiento claro acerca de la manera en que la delincuencia juvenil ha evolucionado en los últimos años y cuáles son los factores de riesgo que la han propiciado, sin esto será difícil determinar las tendencias pero sobre todo actuar atinadamente.
+
+Los esfuerzos realizados hasta hoy no pueden ser demeritados ni los recursos económicos que se emplearon podemos minimizarlos, nuestra preocupación debe ir más allá, es decir, en el sentido de que existan programas y acciones suficientemente financiados pero con los mecanismos de medición adecuados que permitan conocer cuáles han sido sus impactos.
---
+++ lib/Blog/NecesidadDiagnosticoSobreDelincuenciaJuvenil.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * TrcIMPLAN - DESCRIPCION
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Namespace
+namespace Blog;
+
+/**
+ * Clase NecesidadDiagnosticoSobreDelincuenciaJuvenil
+ */
+class NecesidadDiagnosticoSobreDelincuenciaJuvenil extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        // LA NECESIDAD DE UN DIAGNOSTICO SOBRE LA DELINCUENCIA JUVENIL EN EL MUNICIPIO DE TORREÓN
+        $this->nombre           = 'La Necesidad de un Diagnóstico sobre la Delincuencia Juvenil en el Municipio de Torreón';
+        $this->autor            = 'Lic. Alfredo Viesca Domínguez';
+        $this->fecha            = '2015-04-22T09:20';
+        // El nombre del archivo a crear (obligatorio), la ruta a la imagen previa y el encabezado (opcionales). Use minúsculas, números y/o guiones medios.
+        $this->archivo          = 'necesidad-diagnostico-sobre-delincuencia-juvenil';
+        $this->imagen_previa    = 'necesidad-diagnostico-sobre-delincuencia-juvenil/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales. Las categorías son de uso interno.
+        $this->descripcion      = 'Se debe desarrollar un trabajo conjunto entre autoridades y organizaciones civiles en el tema de la delincuencia juvenil para tener un conocimiento claro de cómo ha evulucionado, de su situación real y para crear las políticas públicas pertinentes.';
+        $this->claves           = 'IMPLAN, Torreon, Delincuencia, Juvenil, Autoridad, Organizaciones, Civiles, Politicas, Publicas';
+        $this->categorias       = array('Delincuencia', 'Seguridad');
+        // NO CAMBIE el directorio y el nombre_menu. Están definidos para Análisis Publicados.
+        $this->directorio       = 'blog';
+        $this->nombre_menu      = 'Análisis Publicados';
+        // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
+        $this->estado           = 'publicar';
+        // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
+        $this->para_compartir   = true;
+        // El contenido es estructurado en un esquema
+        $schema                 = new \Base\SchemaBlogPosting();
+        $schema->description    = $this->descripcion;
+        $schema->image          = 'necesidad-diagnostico-sobre-delincuencia-juvenil/imagen.jpg';
+        $schema->name           = $this->nombre;
+        $schema->author         = $this->autor;
+        $schema->datePublished  = $this->fecha;
+        $schema->articleBody    = $this->cargar_archivo_markdown_extra('lib/Blog/NecesidadDiagnosticoSobreDelincuenciaJuvenil.md');
+        // El contenido es una instancia de SchemaBlogPosting
+        $this->contenido        = $schema;
+        // Sin JavaScript
+        $this->javascript       = '';
+    } // constructor
+
+} // Clase NecesidadDiagnosticoSobreDelincuenciaJuvenil
+
+?>
