b941b31ed287bcca798e291cd350bdea14f6dde7
TrcIMPLAN Servidor <webmaster@trcimplan.mx>
2015-04-29 16:45:04 -0500

    Corte al 29/abril 16:45.

--- lib/SMI/Regiones.php
+++ lib/SMI/Regiones.php
@@ -144,6 +144,11 @@ class Regiones extends \Base\Publicacion {
       <li><a href="../indicadores-torreon/seguridad-robos-a-casa-habitacion.html" target="_blank">Robos a Casa Habitación</a></li>
       <li><a href="../indicadores-torreon/seguridad-robos-totales.html" target="_blank">Robos Totales</a></li>
       <li><a href="../indicadores-torreon/seguridad-secuestros.html" target="_blank">Secuestros</a></li>
+      <li><a href="../indicadores-torreon/seguridad-tasa-de-extorsion.html" target="_blank">Tasa de Extorsión</a></li>
+      <li><a href="../indicadores-torreon/seguridad-tasa-de-homicidio-doloso.html" target="_blank">Tasa de Homicidio Doloso</a></li>
+      <li><a href="../indicadores-torreon/seguridad-tasa-de-robo-de-vehiculo-con-violencia.html" target="_blank">Tasa de Robo de Vehículo con Violencia</a></li>
+      <li><a href="../indicadores-torreon/seguridad-tasa-de-robo-de-vehiculo-sin-violencia.html" target="_blank">Tasa de Robo de Vehículo sin Violencia</a></li>
+      <li><a href="../indicadores-torreon/seguridad-tasa-de-secuestro.html" target="_blank">Tasa de Secuestro</a></li>
     </ul>
   </div>
   <div class="col-md-2 indicadores-vinculos">
@@ -297,6 +302,11 @@ class Regiones extends \Base\Publicacion {
       <li><a href="../indicadores-gomez-palacio/seguridad-robos-a-casa-habitacion.html" target="_blank">Robos a Casa Habitación</a></li>
       <li><a href="../indicadores-gomez-palacio/seguridad-robos-totales.html" target="_blank">Robos Totales</a></li>
       <li><a href="../indicadores-gomez-palacio/seguridad-secuestros.html" target="_blank">Secuestros</a></li>
+      <li><a href="../indicadores-gomez-palacio/seguridad-tasa-de-extorsion.html" target="_blank">Tasa de Extorsión</a></li>
+      <li><a href="../indicadores-gomez-palacio/seguridad-tasa-de-homicidio-doloso.html" target="_blank">Tasa de Homicidio Doloso</a></li>
+      <li><a href="../indicadores-gomez-palacio/seguridad-tasa-de-robo-de-vehiculo-con-violencia.html" target="_blank">Tasa de Robo de Vehículo con Violencia</a></li>
+      <li><a href="../indicadores-gomez-palacio/seguridad-tasa-de-robo-de-vehiculo-sin-violencia.html" target="_blank">Tasa de Robo de Vehículo sin Violencia</a></li>
+      <li><a href="../indicadores-gomez-palacio/seguridad-tasa-de-secuestro.html" target="_blank">Tasa de Secuestro</a></li>
     </ul>
   </div>
   <div class="col-md-2 indicadores-vinculos">
@@ -444,6 +454,11 @@ class Regiones extends \Base\Publicacion {
       <li><a href="../indicadores-lerdo/seguridad-robos-a-casa-habitacion.html" target="_blank">Robos a Casa Habitación</a></li>
       <li><a href="../indicadores-lerdo/seguridad-robos-totales.html" target="_blank">Robos Totales</a></li>
       <li><a href="../indicadores-lerdo/seguridad-secuestros.html" target="_blank">Secuestros</a></li>
+      <li><a href="../indicadores-lerdo/seguridad-tasa-de-extorsion.html" target="_blank">Tasa de Extorsión</a></li>
+      <li><a href="../indicadores-lerdo/seguridad-tasa-de-homicidio-doloso.html" target="_blank">Tasa de Homicidio Doloso</a></li>
+      <li><a href="../indicadores-lerdo/seguridad-tasa-de-robo-de-vehiculo-con-violencia.html" target="_blank">Tasa de Robo de Vehículo con Violencia</a></li>
+      <li><a href="../indicadores-lerdo/seguridad-tasa-de-robo-de-vehiculo-sin-violencia.html" target="_blank">Tasa de Robo de Vehículo sin Violencia</a></li>
+      <li><a href="../indicadores-lerdo/seguridad-tasa-de-secuestro.html" target="_blank">Tasa de Secuestro</a></li>
     </ul>
   </div>
   <div class="col-md-2 indicadores-vinculos">
@@ -592,6 +607,11 @@ class Regiones extends \Base\Publicacion {
       <li><a href="../indicadores-matamoros/seguridad-robos-a-casa-habitacion.html" target="_blank">Robos a Casa Habitación</a></li>
       <li><a href="../indicadores-matamoros/seguridad-robos-totales.html" target="_blank">Robos Totales</a></li>
       <li><a href="../indicadores-matamoros/seguridad-secuestros.html" target="_blank">Secuestros</a></li>
+      <li><a href="../indicadores-matamoros/seguridad-tasa-de-extorsion.html" target="_blank">Tasa de Extorsión</a></li>
+      <li><a href="../indicadores-matamoros/seguridad-tasa-de-homicidio-doloso.html" target="_blank">Tasa de Homicidio Doloso</a></li>
+      <li><a href="../indicadores-matamoros/seguridad-tasa-de-robo-de-vehiculo-con-violencia.html" target="_blank">Tasa de Robo de Vehículo con Violencia</a></li>
+      <li><a href="../indicadores-matamoros/seguridad-tasa-de-robo-de-vehiculo-sin-violencia.html" target="_blank">Tasa de Robo de Vehículo sin Violencia</a></li>
+      <li><a href="../indicadores-matamoros/seguridad-tasa-de-secuestro.html" target="_blank">Tasa de Secuestro</a></li>
     </ul>
   </div>
   <div class="col-md-2 indicadores-vinculos">
@@ -735,6 +755,11 @@ class Regiones extends \Base\Publicacion {
       <li><a href="../indicadores-la-laguna/seguridad-robos-a-casa-habitacion.html" target="_blank">Robos a Casa Habitación</a></li>
       <li><a href="../indicadores-la-laguna/seguridad-robos-totales.html" target="_blank">Robos Totales</a></li>
       <li><a href="../indicadores-la-laguna/seguridad-secuestros.html" target="_blank">Secuestros</a></li>
+      <li><a href="../indicadores-la-laguna/seguridad-tasa-de-extorsion.html" target="_blank">Tasa de Extorsión</a></li>
+      <li><a href="../indicadores-la-laguna/seguridad-tasa-de-homicidio-doloso.html" target="_blank">Tasa de Homicidio Doloso</a></li>
+      <li><a href="../indicadores-la-laguna/seguridad-tasa-de-robo-de-vehiculo-con-violencia.html" target="_blank">Tasa de Robo de Vehículo con Violencia</a></li>
+      <li><a href="../indicadores-la-laguna/seguridad-tasa-de-robo-de-vehiculo-sin-violencia.html" target="_blank">Tasa de Robo de Vehículo sin Violencia</a></li>
+      <li><a href="../indicadores-la-laguna/seguridad-tasa-de-secuestro.html" target="_blank">Tasa de Secuestro</a></li>
     </ul>
   </div>
   <div class="col-md-2 indicadores-vinculos">
---
+++ lib/SMIIndicadoresGomezPalacio/SeguridadTasaDeRoboDeVehiculoConViolencia.php
@@ -0,0 +1,315 @@
+<?php
+/**
+ * SeguridadTasaDeRoboDeVehiculoConViolencia.php
+ *
+ * IMPLAN Torreón
+ */
+
+// Namespace
+namespace SMIIndicadoresGomezPalacio;
+
+/**
+ * Clase SeguridadTasaDeRoboDeVehiculoConViolencia
+ */
+class SeguridadTasaDeRoboDeVehiculoConViolencia extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->nombre       = 'Tasa de Robo de Vehículo con Violencia en Gómez Palacio';
+        $this->nombre_menu  = 'Indicadores';
+        $this->directorio   = 'indicadores-gomez-palacio';
+        $this->archivo      = 'seguridad-tasa-de-robo-de-vehiculo-con-violencia';
+        $this->descripcion  = '';
+        $this->claves       = 'Gómez Palacio, Seguridad, Delincuencia';
+        $this->categorias   = array('Seguridad', 'Delincuencia');
+        $this->region_nivel = 111;
+        $this->contenido    = <<<FINAL
+  <ul class="nav nav-tabs lenguetas" id="smi-indicador">
+    <li><a href="#smi-indicador-datos" data-toggle="tab">Datos</a></li>
+    <li><a href="#smi-indicador-otras_regiones" data-toggle="tab">Otras regiones</a></li>
+    <li><a href="#smi-indicador-relacionados" data-toggle="tab">Relacionados</a></li>
+  </ul>
+  <div class="tab-content lengueta-contenido">
+    <div class="tab-pane" id="smi-indicador-datos">
+      <h3>Descripción</h3>
+
+
+      <h3>Información recopilada</h3>
+      <table class="table table-hover table-bordered matriz">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Dato</th>
+            <th>Fuente</th>
+            <th>Notas</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>31/03/2015</td>
+            <td>NO DISPONIBLE</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>El dato proporcionado es estatal. De clic en la pestaña Otras Regiones.</td>
+          </tr>
+        </tbody>
+      </table>
+      <p><b>Unidad:</b> Por cada 100 mil.</p>
+      <h3>Observaciones</h3>
+<p>Fuente: <a href="http://secretariadoejecutivo.gob.mx/incidencia-delictiva/incidencia-delictiva-fuero-comun.php">Secretariado Ejecutivo del Sistema Nacional de Seguridad</a></p>
+
+    </div>
+    <div class="tab-pane" id="smi-indicador-otras_regiones">
+      <h3>Gráfica con los últimos datos de Tasa de Robo de Vehículo con Violencia</h3>
+      <div id="graficaOtrasRegiones" class="grafica"></div>
+      <h3>Últimos datos de Tasa de Robo de Vehículo con Violencia</h3>
+      <table class="table table-hover table-bordered matriz">
+        <thead>
+          <tr>
+            <th>Región</th>
+            <th>Fecha</th>
+            <th>Dato</th>
+            <th>Fuente</th>
+            <th>Notas</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Torreón</td>
+            <td>2015-03-31</td>
+            <td>NO DISPONIBLE</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>El dato proporcionado es estatal. De clic en la pestaña Otras Regiones.</td>
+          </tr>
+          <tr>
+            <td>Gómez Palacio</td>
+            <td>2015-03-31</td>
+            <td>NO DISPONIBLE</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>El dato proporcionado es estatal. De clic en la pestaña Otras Regiones.</td>
+          </tr>
+          <tr>
+            <td>Lerdo</td>
+            <td>2015-03-31</td>
+            <td>NO DISPONIBLE</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>El dato proporcionado es estatal. De clic en la pestaña Otras Regiones.</td>
+          </tr>
+          <tr>
+            <td>Matamoros</td>
+            <td>2015-03-31</td>
+            <td>NO DISPONIBLE</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>El dato proporcionado es estatal. De clic en la pestaña Otras Regiones.</td>
+          </tr>
+          <tr>
+            <td>La Laguna</td>
+            <td>2015-03-31</td>
+            <td>NO DISPONIBLE</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>El dato proporcionado es estatal. De clic en la pestaña Otras Regiones.</td>
+          </tr>
+          <tr>
+            <td>Coahuila</td>
+            <td>2015-03-31</td>
+            <td>1.8900</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>Fuente: [Secretariado Ejecutivo del Sistema Nacional de Seguridad](http://secretariadoejecutivo.gob.mx/incidencia-delictiva/incidencia-delictiva-fuero-comun.php)</td>
+          </tr>
+          <tr>
+            <td>Durango</td>
+            <td>2015-03-31</td>
+            <td>4.5900</td>
+            <td>Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública</td>
+            <td>Fuente: [Secretariado Ejecutivo del Sistema Nacional de Seguridad](http://secretariadoejecutivo.gob.mx/incidencia-delictiva/incidencia-delictiva-fuero-comun.php)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="tab-pane" id="smi-indicador-relacionados">
+      <h3>Páginas relacionadas con Tasa de Robo de Vehículo con Violencia</h3>
+      <table class="table table-hover table-bordered matriz">
+        <thead>
+          <tr>
+            <th>Creado</th>
+            <th>Tipo</th>
+            <th>Nombre</th>
+            <th>Descripción</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>22/04/2015</td>
+            <td>Análisis</td>
+            <td><a href="../blog/necesidad-diagnostico-sobre-delincuencia-juvenil.html">La Necesidad de un Diagnóstico sobre la Delincuencia Juvenil en el Municipio de Torreón</a></td>
+            <td>Se debe desarrollar un trabajo conjunto entre autoridades y organizaciones civiles en el tema de la delincuencia juvenil para tener un conocimiento claro de cómo ha evulucionado, de su situación real y para crear las políticas públicas pertinentes.</td>
+          </tr>
+          <tr>
+            <td>17/02/2015</td>
+            <td>Análisis</td>
+            <td><a href="../blog/seguridad-humana.html">Seguridad Humana</a></td>
+            <td>La población se siente insegura no solo por las acciones de la delincuencia organizada, también la genera la falta de empleo, la pobreza persistente, la ausencia de acceso a cuidados básicos de salud, la violencia física, la violencia familiar, la represión política, la violación a los derechos humanos, el deterioro ambiental, la incertidumbre política, la falta de educación, la imposibilidad de ejercer el derecho de libre expresión y la ausencia de equidad de género entre otros.</td>
+          </tr>
+          <tr>
+            <td>07/01/2015</td>
+            <td>Análisis</td>
+            <td><a href="../blog/prevencion-delincuencia-juvenil.html">La prevención de la Delincuencia Juvenil, elemento central de la prevención integral del delito y la violencia</a></td>
+            <td>Dentro de una estrategia de prevención integral del delito, la delincuencia juvenil constituye desde luego el elemento fundamental, si lo que se pretende es evitar que niños de doce años decidan formar parte de la delincuencia.</td>
+          </tr>
+          <tr>
+            <td>18/11/2014</td>
+            <td>Análisis</td>
+            <td><a href="../blog/prevencion-delito-seguridad.html">La Prevención del Delito como elemento fundamental para la Seguridad</a></td>
+            <td>La prevención del delito debe ser el elemento central en la búsqueda de la seguridad en la Zona Metropolitana de la Laguna (ZML).</td>
+          </tr>
+          <tr>
+            <td>25/09/2014</td>
+            <td>Análisis</td>
+            <td><a href="../blog/comportamiento-tendencia-incidencia-delictiva.html">Comportamiento y tendencia de la incidencia delictiva. Una visión ampliada.</a></td>
+            <td>Mostramos las tendencias de los delitos más recurrentes en la ciudad de Torreón; con datos del Secretariado Ejecutivo del Sistema Nacional de Seguridad Pública.</td>
+          </tr>
+          <tr>
+            <td>02/06/2014</td>
+            <td>Análisis</td>
+            <td><a href="../blog/zml-sistema-derecho-confiable-objetivo.html">La Zona Metropolitana de La Laguna y sus municipios en Sistema de Derecho Confiable y Objetivo</a></td>
+            <td>De acuerdo al ICU 2012, es el subíndice más débil de la región; algunos de sus subíndices ponderados con alto peso por el IMCO corresponden a rubros de seguridad pública.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-delitos.html">Delitos en Gómez Palacio</a></td>
+            <td>Total de delitos cometidos.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-homicidios.html">Homicidios en Gómez Palacio</a></td>
+            <td>Número de homicidios por mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-presuntos-delincuentes-del-fuero-comun.html">Presuntos Delincuentes del Fuero Común en Gómez Palacio</a></td>
+            <td>Cantidad de presuntos delincuentes registrados en los juzgados de primera instancia del fuero común por año.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-presuntos-delincuentes-del-fuero-federal.html">Presuntos Delincuentes del Fuero Federal en Gómez Palacio</a></td>
+            <td>Presuntos delincuentes registrados en los juzgados de primera instancia en materia penal del fuero federal</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-presuntos-delincuentes-por-narcoticos.html">Presuntos Delincuentes por Narcóticos en Gómez Palacio</a></td>
+            <td>Presuntos delincuentes por delitos en materia de narcóticos registrados en los juzgados del fuero federal.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-robo-a-negocios.html">Robo a Negocios en Gómez Palacio</a></td>
+            <td>Robos totales por mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-robo-a-transeuntes.html">Robo a Transeuntes en Gómez Palacio</a></td>
+            <td>Robos a Transeuntes totales por mes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-robo-de-vehiculos.html">Robo de Vehículos en Gómez Palacio</a></td>
+            <td>Robo de vehículos con violencia y sin violencia cometidos en el mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-robos-totales.html">Robos Totales en Gómez Palacio</a></td>
+            <td>Cantidad total de robos en el mes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-robos-a-casa-habitacion.html">Robos a Casa Habitación en Gómez Palacio</a></td>
+            <td>Robos totales por mes a casa habitación.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-secuestros.html">Secuestros en Gómez Palacio</a></td>
+            <td>Cantidad de secuestros en el mes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-sentenciados-registrados-del-fuero-comun.html">Sentenciados Registrados del Fuero Común en Gómez Palacio</a></td>
+            <td>Cantidad de sentenciados registrados en los juzgados de primera instancia del fuero común por año.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-sentenciados-registrados-del-fuero-federal.html">Sentenciados Registrados del Fuero Federal en Gómez Palacio</a></td>
+            <td>Sentenciados registrados en los juzgados de primera instancia en materia penal del fuero federal</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-sentenciados-en-materia-de-narcoticos-registrados-en-el-fuero-federal.html">Sentenciados en Materia de Narcóticos Registrados en el Fuero Federal en Gómez Palacio</a></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-tasa-de-delitos.html">Tasa de Delitos en Gómez Palacio</a></td>
+            <td>Delitos por cada 100 mil habitantes</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-tasa-de-homicidios.html">Tasa de Homicidios en Gómez Palacio</a></td>
+            <td>Número de homicidios por cada 100 mil habitantes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-tasa-de-robo-de-vehiculos.html">Tasa de Robo de VehÍculos en Gómez Palacio</a></td>
+            <td>Cantidad de robos de vehículos por cada 100 mil habitantes.</td>
+          </tr>
+          <tr>
+            <td>ND</td>
+            <td>Indicador</td>
+            <td><a href="../indicadores-gomez-palacio/seguridad-tasa-de-secuestro.html">Tasa de Secuestro en Gómez Palacio</a></td>
+            <td>Homicidios por habitantes por 100 mil</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+FINAL;
+        $this->javascript   = <<<FINAL
+// LENGUETA smi-indicador-otras_regiones
+$('#smi-indicador a[href="#smi-indicador-otras_regiones"]').on('shown.bs.tab', function(e){
+  // Gráfica
+  if (typeof vargraficaOtrasRegiones === 'undefined') {
+    vargraficaOtrasRegiones = Morris.Bar({
+      element: 'graficaOtrasRegiones',
+      data: [{ region: 'Torreón', dato: 0 },{ region: 'Gómez Palacio', dato: 0 },{ region: 'Lerdo', dato: 0 },{ region: 'Matamoros', dato: 0 },{ region: 'La Laguna', dato: 0 },{ region: 'Coahuila', dato: 1.8900 },{ region: 'Durango', dato: 4.5900 }],
+      xkey: 'region',
+      ykeys: ['dato'],
+      labels: ['Dato'],
+      barColors: ['#FF5B02']
+    });
+  }
+});
+// TWITTER BOOTSTRAP TABS, ESTABLECER QUE LA LENGÜETA ACTIVA ES smi-indicador-datos
+$(document).ready(function(){
+  $('#smi-indicador a[href="#smi-indicador-datos"]').tab('show')
+});
+FINAL;
+    } // constructor
+
+} // Clase SeguridadTasaDeRoboDeVehiculoConViolencia
+
+?>
