6e289a6835c16f04eefda814b784c43a25263ba3
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-05-20 12:56:51 -0500

    Agregué análisis Indicadores Económicos Primer Trimestre 2015 Torreón.

---
+++ lib/Blog/IndicadoresEconomicosPrimerTrimestre2015Torreon.md
@@ -0,0 +1,67 @@
+
+Los indicadores de inflación, apertura de empresas, trabajadores asegurados y salario promedio, para el municipio de Torreón muestran una tendencia positiva en sus resultados del primer trimestre de 2015 respecto al mismo periodo del 2014.
+
+<div style="clear:left;"></div>
+
+### Inflación
+
+La inflación anual a marzo de 2015 fue de 2.97% en Torreón, mientras que a nivel nacional fue de 3.14%.
+
+La variación porcentual en el Índice Nacional de Precios al Consumidor (INPC) en Torreón fue de 0.38% en el primer trimestre de 2015, menor a la inflación acumulada durante el primer trimestre de 2014.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/01-inflacion.png" alt="Inflacion">
+
+FUENTE: INEGI
+
+### Trabajadores asegurados
+
+Entre febrero de 2014 y febrero de 2015 los trabajadores asegurados en el Instituto Mexicano del Seguro Social (IMSS) aumentaron en 8,944, significando un aumento de 5.5%. Tan solo durante el primer bimestre de éste año se registraron 2,419 empleos formales, mientras que en enero y febrero de 2014 se registraron 950.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/02-trabajadores-asegurados.png" alt="Trabajadores Asegurados">
+
+FUENTE: Instituto Mexicano del Seguro Social
+
+### Nuevas Empresas
+
+Durante el primer trimestre de 2015 se abrieron 544 nuevas empresas, más del doble de las 266 registradas en el primer trimestre de 2014.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/03-nuevas-empresas.png" alt="Nuevas Empresas">
+
+FUENTE: Dirección General de Desarrollo Económico. Ventanilla Universal
+
+### Salarios
+
+El salario promedio del municipio de Torreón se mantuvo por encima de $ 6,400 y superior a la media nacional de $ 6,035.69 en 2015.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/04-salario-promedio.png" alt="Salario Promedio">
+
+FUENTE: Encuesta Nacional de Ocupación y Empleo de INEGI
+
+NOTA(S): Salario promedio mensual para personal ocupado de jornada laboral de 35 a 48 horas semanales
+
+Tanto el aumento en las empresas registradas como el aumento en empleos formales hablan de un mayor dinamismo en la economía local, lo que a su vez influye en el aumento de precios y salarios.
+
+Para tener una idea mejor del desempeño de Torreón, comparamos al municipio con la media nacional y ciudades con características similares y encontramos:
+
+* Torreón tiene un salario promedio superior a la media nacional y menor al de las ciudades de Saltillo, Chihuahua y San Luis Potosí debido a un pequeño aumento en el promedio de Torreón contra aumentos más considerables en otras ciudades durante el último trimestre.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/05-salario-promedio-primer-trimestre-2015.png" alt="Salario Promedio primer trimestre 2015">
+
+FUENTE: Encuesta Nacional de Ocupación y Empleo de INEGI
+
+NOTA(S): Salario promedio mensual para personal ocupado de jornada laboral de 35 a 48 horas semanales
+
+* La inflación acumulada de nuestro municipio fue menor a la nacional y a la de ciudades como Chihuahua y León.
+* La inflación anual de Torreón fue menor a la nacional y menor a la de Chihuahua, aunque mayor a la de León y San Luis Potosí.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/06-inflacion-primer-trimestre-2015.png" alt="Inflacion primer trimestre 2015">
+
+FUENTE: INEGI
+
+* En Torreón se generaron tantos empleos como en Chihuahua y más empleos que en las ciudades de Saltillo y León, esta última habiendo tenido resultados negativos.
+
+<img class="img-responsive" src="indicadores-economicos-primer-trimestre-2015-torreon/07-aumento-en-trabajadores-asegurados.png" alt="Aumento en Trabajadores Asegurados primer bimestre 2015">
+
+FUENTE: Instituto Mexicano del Seguro Social
+
+NOTA: Datos no disponibles para el municipio de S.L.P.
---
+++ lib/Blog/IndicadoresEconomicosPrimerTrimestre2015Torreon.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * TrcIMPLAN - DESCRIPCION
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Namespace
+namespace Blog;
+
+/**
+ * Clase IndicadoresEconomicosPrimerTrimestre2015Torreon
+ */
+class IndicadoresEconomicosPrimerTrimestre2015Torreon extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre          = 'Indicadores Económicos del primer trimestre de 2015 para el Municipio de Torreón';
+        $this->autor           = 'Lic. Alicia Valdez Ibarra';
+        $this->fecha           = '2015-05-20T12:50';
+        // El nombre del archivo a crear (obligatorio) y rutas relativas a las imágenes. Use minúsculas, números y/o guiones medios
+        $this->archivo         = 'indicadores-economicos-primer-trimestre-2015-torreon';
+        $this->imagen          = 'indicadores-economicos-primer-trimestre-2015-torreon/imagen.jpg';
+        $this->imagen_previa   = 'indicadores-economicos-primer-trimestre-2015-torreon/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales. Las categorías son de uso interno
+        $this->descripcion     = 'Análisis de los indicadores de inflación, apertura de empresas, trabajadores asegurados y salario promedio para el municipio de Torreón.';
+        $this->claves          = 'IMPLAN, Torreon, Inflacion, Apertura, Empresas, Trabajadores, Asegurados, Salario, Promedio';
+        $this->categorias      = array('Empleo', 'Empresas', 'Mercados');
+        // El directorio en la raíz donde se guardará el archivo HTML
+        $this->directorio      = 'blog';
+        // Opción del menú Navegación a poner como activa cuando vea esta publicación
+        $this->nombre_menu     = 'Análisis Publicados';
+        // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
+        $this->estado          = 'publicar';
+        // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
+        $this->para_compartir  = true;
+        // El contenido es estructurado en un esquema
+        $schema                = new \Base\SchemaBlogPosting();
+        $schema->description   = $this->descripcion;
+        $schema->image         = $this->imagen;
+     // $schema->image_show    = true; // Por defecto la imagen se agrega al principio del contenido
+        $schema->name          = $this->nombre;
+        $schema->author        = $this->autor;
+        $schema->datePublished = $this->fecha;
+        $schema->articleBody   = $this->cargar_archivo_markdown_extra('lib/Blog/IndicadoresEconomicosPrimerTrimestre2015Torreon.md');
+        // El contenido es una instancia de SchemaBlogPosting
+        $this->contenido       = $schema;
+        // Sin JavaScript
+        $this->javascript      = '';
+        // Para redifusión, si tiene la imagen, se pone la imagen y después el contenido
+        if ($this->imagen != '') {
+            $this->redifusion  = "<img src=\"{$this->imagen}\"><br>\n\n{$schema->articleBody}";
+        } else {
+            $this->redifusion  = $schema->articleBody;
+        }
+    } // constructor
+
+} // Clase IndicadoresEconomicosPrimerTrimestre2015Torreon
+
+?>
