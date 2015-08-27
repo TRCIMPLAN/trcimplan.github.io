be56d643d7429ec27e1b6d579eb8ebec0d532741
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-08-27 14:40:23 -0500

    Publiqué el último análisis.

--- lib/Blog/CincoClavesParaEntenderLaMovilidadNoMotorizada.php
+++ lib/Blog/CincoClavesParaEntenderLaMovilidadNoMotorizada.php
@@ -34,7 +34,7 @@ class CincoClavesParaEntenderLaMovilidadNoMotorizada extends \Base\Publicacion {
         // Título, autor y fecha
         $this->nombre          = 'Cinco Claves para entender la Movilidad No Motorizada';
         $this->autor           = 'Arq. Cecilio Pedro Secunza Schott';
-        $this->fecha           = '2015-08-27T12:10';
+        $this->fecha           = '2015-08-27T14:30';
         // El nombre del archivo a crear (obligatorio) y rutas relativas a las imágenes
         $this->archivo         = 'cinco-claves-para-entender-la-movilidad-no-motorizada';
         $this->imagen          = 'cinco-claves-para-entender-la-movilidad-no-motorizada/imagen.jpg';
@@ -47,7 +47,7 @@ class CincoClavesParaEntenderLaMovilidadNoMotorizada extends \Base\Publicacion {
         // Opción del menú Navegación a poner como activa cuando vea esta publicación
         $this->nombre_menu     = 'Análisis Publicados';
         // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
-        $this->estado          = 'revisar';
+        $this->estado          = 'publicar';
         // Indicar que NO se vaya a poner la imagen en la página y en la redifusión. Por defecto es verdadero.
         $this->poner_imagen_en_contenido = false;
         // El contenido es estructurado en un esquema
