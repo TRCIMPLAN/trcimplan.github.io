91defa8da9e5e6ec5e98c8c5a0b7a27a1e65103e
TrcIMPLAN Servidor <webmaster@trcimplan.mx>
2015-10-04 20:19:45 -0500

    Corte para ver por primera vez smi/datos-abiertos.html

---
+++ lib/SMI/DatosAbiertos.md
@@ -0,0 +1,22 @@
+### Descargue los datos del SMI
+
+[Archivo ZIP con los datos del Sistema Metropolitano de Indicadores](http://www.trcimplan.gob.mx/smi/trcimplan-smi.zip)
+
+Detalles del archivo:
+
+* Archivo de texto separado por comas CSV comprimido con ZIP.
+* Codificación de caracteres UTF-8.
+* Primeria línea con los nombres de las columnas.
+* Todas las letras se han cambiado a mayúsculas y se les han removido los acentos. Excepto la eñe (Ñ) y la diéresis (Ü).
+
+### ¿Qué son los datos abiertos?
+
+Pendiente.
+
+### ¿Cual es el fin de publicarlos?
+
+Pendiente.
+
+### ¿Cómo puedo usarlos?
+
+Pendiente.
---
+++ lib/SMI/DatosAbiertos.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * TrcIMPLAN - SMI Datos Abiertos (Creado por Central:SmiLanzadera)
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace SMI;
+
+/**
+ * Clase DatosAbiertos
+ */
+class DatosAbiertos extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre          = 'Datos Abiertos del SMI';
+        $this->autor           = 'Dirección de Investigación Estratégica';
+        $this->fecha           = '1980-01-01T08:00';
+        // El nombre del archivo a crear (obligatorio) y rutas relativas a las imágenes
+        $this->archivo         = 'datos-abiertos';
+        $this->imagen          = '../smi/introduccion/imagen.jpg';
+        $this->imagen_previa   = '../smi/introduccion/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion     = 'Pendiente.';
+        $this->claves          = 'IMPLAN, Torreon, Datos Abiertos, Indicadores';
+        // El directorio en la raíz donde se guardará el archivo HTML
+        $this->directorio      = 'smi';
+        // Opción del menú Navegación a poner como activa cuando vea esta publicación
+        $this->nombre_menu     = 'Indicadores > Datos Abiertos';
+        // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
+        $this->estado          = 'revisar';
+        // Indicar que NO se vaya a poner la imagen en la página y en la redifusión
+        $this->poner_imagen_en_contenido = false;
+        // El contenido es estructurado en un esquema
+        $schema                = new \Base\SchemaArticle();
+        $schema->name          = $this->nombre;
+        $schema->description   = $this->descripcion;
+        $schema->datePublished = $this->fecha;
+        $schema->image         = $this->imagen;
+        $schema->image_show    = $this->poner_imagen_en_contenido;
+        $schema->author        = $this->autor;
+        // El contenido es una instancia de SchemaArticle
+        $this->contenido       = $schema;
+        // Se define una ruta a una archivo markdown para que cuando se ejecute el método HTML se cargue
+        $this->contenido_archivo_markdown = 'lib/SMI/DatosAbiertos.md';
+        // Para el Organizador
+        $this->categorias      = array();
+        $this->fuentes         = array();
+        $this->regiones        = array();
+    } // constructor
+
+} // Clase DatosAbiertos
+
+?>
