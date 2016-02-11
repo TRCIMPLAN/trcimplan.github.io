abb2ce9032c2dc0cac54e8ba8d232ab8ab978c1c
TrcIMPLAN Servidor <webmaster@trcimplan.mx>
2016-02-11 14:56:44 -0600

    Nuevo SIG Torreón Polígonos de Pobreza.

---
+++ lib/SIGMapasTorreon/PoligonosPobreza.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * TrcIMPLAN - SIG Mapas Polígonos de Pobreza
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Namespace
+namespace SIGMapasTorreon;
+
+/**
+ * Clase PoligonosPobreza
+ */
+class PoligonosPobreza extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre             = 'Polígonos de Pobreza';
+        $this->autor              = 'Arq. Daniela Patricia Corral Hernández';
+        $this->fecha              = '2016-02-11T14:00';
+        // El nombre del archivo a crear (obligatorio) y rutas relativas a las imágenes. Use minúsculas, números y/o guiones medios
+        $this->archivo            = 'poligonos-pobreza';
+        $this->imagen             = 'poligonos-pobreza/imagen.jpg';
+        $this->imagen_previa      = 'poligonos-pobreza/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales. Las categorías son de uso interno
+        $this->descripcion        = 'Localización y demarcación de los territorios que presentan una mayor situación de pobreza y de rezago, conocidos como polígonos de pobreza.';
+        $this->claves             = 'IMPLAN, Torreon, Poligonos, Pobreza';
+        $this->categorias         = array('Bienestar', 'Población');
+        // El nombre del directorio en la raíz del sitio donde se escribirá el archivo HTML
+        $this->directorio         = 'sig-mapas-torreon';
+        // Opción del menú Navegación a poner como activa cuando vea esta publicación
+        $this->nombre_menu        = 'Información Geográfica';
+        // El estado puede ser 'publicar' (crear HTML y agregarlo a índices/galerías), 'revisar' (sólo crear HTML y accesar por URL) o 'ignorar'
+        $this->estado             = 'Publicar';
+        // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
+        $this->para_compartir     = true;
+        // Para el botón de ver a pantalla completa
+        $this->url                = 'https://implantorreon.cartodb.com/u/sigimplan/viz/d4ef8bb0-9ea6-11e5-8b0e-0e5db1731f59/embed_map';
+        $this->url_etiqueta       = 'Ver a pantalla completa';
+        // Instancia de SchemaPostalAddress que tiene la localidad, municipio y país
+        $region                   = new \Base\SchemaPostalAddress();
+        $region->addressCountry   = 'MX';
+        $region->addressRegion    = 'Coahuila de Zaragoza';
+        $region->addressLocality  = 'Torreón';
+        // Instancia de SchemaMapa con el mapa en CartoDB
+        $mapa                     = new \Base\SchemaMap();
+        $mapa->mapType            = 'VenueMap';
+        $mapa->url                = $this->url;
+        $mapa->url_label          = $this->url_etiqueta;
+        $mapa->theMap             = <<<FINAL
+<iframe width='100%' height='520' frameborder='0' src='https://implantorreon.cartodb.com/u/sigimplan/viz/d4ef8bb0-9ea6-11e5-8b0e-0e5db1731f59/embed_map' allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+<p><b>VERSIÓN DE DIVULGACIÓN E INFORMACIÓN, NO PRODUCE EFECTOS JURÍDICOS.</b></p>
+FINAL;
+        // Instancia de SchemaPlace agrupa la región y el mapa
+        $lugar                    = new \Base\SchemaPlace();
+        $lugar->address           = $region;
+        $lugar->hasMap            = $mapa;
+        // Instancia de SchemaCreativeWork lo empaca con más datos
+        $paquete                  = new \Base\SchemaCreativeWork();
+        $paquete->big_heading     = true;
+        $paquete->name            = $this->nombre;
+        $paquete->description     = $this->descripcion;
+        $paquete->author          = $this->autor;
+        $paquete->datePublished   = $this->fecha;
+        $paquete->headline_style  = $this->encabezado_color;
+        $paquete->image           = $this->imagen;
+     // $paquete->image_show      = false; // Por defecto la imagen NO se agrega al principio del contenido
+        $paquete->contentLocation = $lugar;
+        // El contenido es una instancia de SchemaCreativeWork
+        $this->contenido          = $paquete;
+    } // constructor
+
+    /**
+     * Javascript
+     *
+     * @return string No hay código Javascript, entrega un texto vacío
+     */
+    public function javascript() {
+        // JavaScript
+        // Sin caja JS
+        // Ejecutar este método en el padre
+        return parent::javascript();
+    } // javascript
+
+    /**
+     * Redifusion HTML
+     *
+     * @return string Código HTML
+     */
+    public function redifusion_html() {
+        // Para redifusión, si tiene una imagen, se pone la imagen y después el contenido
+        if ($this->imagen != '') {
+            $this->redifusion = sprintf("<a href=\"%s\"><img src=\"%s\"><br>\n\n%s</a>", "{$this->archivo}.html", $this->imagen, $this->descripcion);
+        } else {
+            $this->redifusion = sprintf('<a href="%s">%s</a>', "{$this->archivo}.html", $this->descripcion);
+        }
+        // Ejecutar este método en el padre
+        return parent::redifusion_html();
+    } // redifusion_html
+
+} // Clase PoligonosPobreza
+
+?>
