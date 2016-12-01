845949fc95d88ea261dac54df3f3884cb298676f
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2016-12-01 10:12:39 -0600

    Corregí error en javascript de twitter timeline.

--- lib/Configuracion/PaginaInicialConfig.php
+++ lib/Configuracion/PaginaInicialConfig.php
@@ -249,7 +249,7 @@ class PaginaInicialConfig extends \Base\Plantilla {
         // Acumular javascipt del Twitter Timeline
         $this->javascript[] = <<<FINAL
   // Twitter timeline
-  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 FINAL;
     } // ultimas_publicaciones
 
