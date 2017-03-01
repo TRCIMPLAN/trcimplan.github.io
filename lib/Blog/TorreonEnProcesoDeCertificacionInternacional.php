69a423b6ed7643e98735442ac9cef1c999a29893
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2017-03-01 16:05:49 -0600

    Nuevo análisis Torreón en Proceso de Certificacion internacional.

---
+++ lib/Blog/TorreonEnProcesoDeCertificacionInternacional.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - Torreón en proceso de certificación internacional
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace Blog;
+
+/**
+ * Clase TorreonEnProcesoDeCertificacionInternacional
+ */
+class TorreonEnProcesoDeCertificacionInternacional extends \Base\PublicacionSchemaBlogPosting {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre                     = 'Torreón en proceso de certificación internacional';
+        $this->autor                      = 'Lic. Luis A. Gutiérrez Arizpe';
+        $this->fecha                      = '2017-03-01T15:00';
+        // El nombre del archivo a crear
+        $this->archivo                    = 'torreon-en-proceso-de-certificacion-internacional';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion                = 'Torreón está en proceso de certificación de la norma ISO 37120:2014 Desarrollo Sostenible de Ciudades por parte del Consejo Mundial de Información de las Ciudades (WCCD). Nuestra ciudad se medirá con los estándares internacionales que buscan elevar la calidad de vida de la población.';
+        $this->claves                     = 'IMPLAN, Torreon, Certificacion, Ciudades, WCCD';
+        // Opción de navegación a poner como activa
+        $this->nombre_menu                = 'Análisis Publicados';
+        // Ruta al archivo markdown con el contenido
+        $this->contenido_archivo_markdown = 'lib/Blog/TorreonEnProcesoDeCertificacionInternacional.md';
+        // Banderas
+        $this->poner_imagen_en_contenido  = TRUE;
+        $this->para_compartir             = TRUE;
+        // Para el Organizador
+        $this->categorias                 = array('Bienestar');
+        $this->fuentes                    = array('IMCO', 'IMPLAN', 'ONU-Habitat');
+        $this->regiones                   = array('Torreón');
+    } // constructor
+
+} // Clase TorreonEnProcesoDeCertificacionInternacional
+
+?>
