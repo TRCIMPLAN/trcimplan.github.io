bc2cce1255217f85b7422ebf144243126a82bd6d
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2017-06-21 14:58:08 -0500

    Avance en nuevo SMIBase ahora en SMIBaseNUEVO.

---
+++ lib/SMIBaseNUEVO/GraficaLineasWeb.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * TrcIMPLAN SMIBase - GraficaLineasWeb
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace SMIBase;
+
+/**
+ * Clase GraficaLineasWeb
+ */
+class GraficaBarrasWeb extends Grafica implements SalidaWeb {
+
+    // protected $identificador;
+
+    /**
+     * HTML
+     *
+     * @return string Código HTML
+     */
+    public function html() {
+    } // html
+
+    /**
+     * Javascript
+     *
+     * @return string Código Javascript
+     */
+    public function javascript() {
+    } // javascript
+
+} // Clase GraficaLineasWeb
+
+?>
