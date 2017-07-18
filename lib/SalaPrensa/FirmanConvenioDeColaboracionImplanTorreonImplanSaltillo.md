69ed84af0232ebb941413e4cc159576f0acd79e2
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2017-07-18 13:33:06 -0500

    Sala de Prensa: IMPLAN Saltillo.

---
+++ lib/SalaPrensa/FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo.md
@@ -0,0 +1,14 @@
+
+El Instituto Municipal de Planeación y Competitividad de Torreón, recibió la visita el pasado 7 de julio de parte del equipo del **Instituto Municipal de Planeación de Saltillo,** para establecer una colaboración conjunta en temas comunes, como es la planeación urbana, la elaboración de un Atlas de Riesgo y proyectos ambientales.
+
+La reunión contó con la presencia del **director del IMPLAN Saltillo, Ariel Domínguez y de su coordinador jurídico José Ruiz Fernández,** así como del director del IMPLAN Torreón, Eduardo Holguín y parte de su equipo de trabajo.
+
+**Ambas instituciones firmaron un convenio de colaboración para trabajar en temas como el Atlas de Riesgo y otros proyectos que tienen que ver con la planeación y desarrollo de ambas ciudades en el ámbito urbano, ecológico y social.** Asimismo los directores intercambiaron experiencias sobre sus mecanismos de operación, con el fin de fortalecer a futuro ambas instituciones.
+
+**También se contó con la presencia de la directora de Medio Ambiente Susana Esténs,** quien intercambió experiencias con el IMPLAN Saltillo, en temas de monitoreo de la calidad del aire y proyectos de infraestructura verde.
+
+Los directores del IMPLAN Saltillo y del IMPLAN Torreón, reconocieron la importancia de que dichos institutos sean reconocidos como los entes más importantes de la planeación municipal y metropolitana.
+
+<img class="img-responsive" src="2017-07-18-firman-convenio-de-colaboracion-implan-torreon-implan-saltillo/foto-1.jpg" alt="Foto 1">
+
+<img class="img-responsive" src="2017-07-18-firman-convenio-de-colaboracion-implan-torreon-implan-saltillo/foto-2.jpg" alt="Foto 2">
---
+++ lib/SalaPrensa/FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace SalaPrensa;
+
+/**
+ * Clase FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo
+ */
+class FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo extends \Base\PublicacionSchemaBlogPosting {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Ejecutar constructor en el padre
+        parent::__construct();
+        // Título, autor y fecha
+        $this->nombre                     = 'Firman convenio de colaboración IMPLAN Torreón – IMPLAN Saltillo';
+        $this->fecha                      = '2017-07-18T13:20';
+        // El nombre del archivo a crear
+        $this->archivo                    = '2017-07-18-firman-convenio-de-colaboracion-implan-torreon-implan-saltillo';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion                = 'IMPLA Torreón recibió la visita de personal de IMPLAN Saltillo, además de la D.G. de Medio Ambiente de Torreón y firmaron convenios de colaboración para hacer el Atlas de Riesgo y otros proyectos más.';
+        $this->claves                     = 'IMPLAN, Torreon, Saltillo, Colaboración, Atlas de Riesgos';
+        // Ruta al archivo markdown con el contenido
+        $this->contenido_archivo_markdown = 'lib/SalaPrensa/FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo.md';
+        // Para el Organizador
+        $this->categorias                 = array('Seguridad', 'Recursos Naturales');
+        $this->fuentes                    = array();
+        $this->regiones                   = array('Torreón');
+    } // constructor
+
+} // Clase FirmanConvenioDeColaboracionImplanTorreonImplanSaltillo
+
+?>
