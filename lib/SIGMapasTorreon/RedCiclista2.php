5db598880006734900c38b0b415360de91950e90
Victor Gomez <vmparadigma@hotmail.com>
2018-07-06 13:01:11 -0500

    Modificación de imagen de Artículo

---
+++ lib/SIGMapasTorreon/RedCiclista2.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - SIGMapasTorreon RedCiclista2
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace SIGMapasTorreon;
+
+/**
+ * Clase RedCiclista2
+ */
+class RedCiclista2 extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre             = 'red-ciclista2';
+        $this->autor              = 'Arq. Cecilio Pedro Secunza Schott';
+        $this->fecha              = '2018-07-04T13:46';
+        // El nombre del archivo a crear y rutas relativas a las imágenes
+        $this->archivo            = 'red-ciclista2';
+        $this->imagen             = 'red-ciclista2/imagen.jpg';
+        $this->imagen_previa      = 'red-ciclista2/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion        = 'Otra versión de la red ciclista';
+        $this->claves             = 'IMPLAN';
+        // El directorio en la raíz donde se guardará el archivo HTML
+        $this->directorio         = 'sig-mapas-torreon';
+        // Opción del menú Navegación a poner como activa cuando vea esta publicación
+        $this->nombre_menu        = 'Información Geográfica';
+        // El estado puede ser publicar, revisar o ignorar
+        $this->estado             = 'Revisar';
+        // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
+        $this->para_compartir     = FALSE;
+        // Para el Organizador
+        $this->categorias         = array('Infraestructura');
+        // Para el botón de ver a pantalla completa
+        // Sin URL
+        $this->url_etiqueta       = 'Ver a pantalla completa';
+        // Instancia de SchemaPostalAddress que tiene la localidad, municipio y país
+        $region                   = new \Base\SchemaPostalAddress();
+        $region->addressCountry   = 'MX';
+        $region->addressRegion    = 'Coahuila de Zaragoza';
+        $region->addressLocality  = 'Torreón';
+        // Instancia de SchemaMapa con el mapa en CartoDB
+        $mapa                     = new \Base\SchemaMap();
+        $mapa->mapType            = 'VenueMap';
+        $mapa->url                = $this->url;
+        $mapa->url_label          = $this->url_etiqueta;
+        $mapa->theMap             = <<<FINAL
+<iframe src="https://www.google.com/maps/d/embed?mid=1K5Bzaltg5F2zaxSXnyVk7GKiWAihYFo8" width="640" height="480"></iframe>
+FINAL;
+        // Instancia de SchemaPlace agrupa la región y el mapa
+        $lugar                    = new \Base\SchemaPlace();
+        $lugar->address           = $region;
+        $lugar->hasMap            = $mapa;
+        // Instancia de SchemaCreativeWork lo empaca con más datos
+        $paquete                  = new \Base\SchemaCreativeWork();
+        $paquete->big_heading     = true;
+        $paquete->name            = $this->nombre;
+        $paquete->description     = $this->descripcion;
+        $paquete->author          = $this->autor;
+        $paquete->datePublished   = $this->fecha;
+        $paquete->headline_style  = $this->encabezado_color;
+        $paquete->image           = $this->imagen;
+        $paquete->contentLocation = $lugar;
+        // El contenido es una instancia de SchemaCreativeWork
+        $this->contenido          = $paquete;
+    } // constructor
+
+    /**
+     * Javascript
+     *
+     * @return string No hay código Javascript, entrega un texto vacío
+     */
+    public function javascript() {
+        // JavaScript
+        // Sin caja JS
+        // Ejecutar este método en el padre
+        return parent::javascript();
+    } // javascript
+
+    /**
+     * Redifusion HTML
+     *
+     * @return string Código HTML
+     */
+    public function redifusion_html() {
+        // Para redifusión, si tiene una imagen, se pone la imagen y después el contenido
+        if ($this->imagen != '') {
+            $this->redifusion = sprintf("<a href=\"%s\"><img src=\"%s\"><br>\n\n%s</a>", "{$this->archivo}.html", $this->imagen, $this->descripcion);
+        } else {
+            $this->redifusion = sprintf('<a href="%s">%s</a>', "{$this->archivo}.html", $this->descripcion);
+        }
+        // Ejecutar este método en el padre
+        return parent::redifusion_html();
+    } // redifusion_html
+
+} // Clase RedCiclista2
+
+?>
---
+++ lib/SIGMapasTorreon/TorreonJardin.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - SIGMapasTorreon TorreonJardin
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace SIGMapasTorreon;
+
+/**
+ * Clase TorreonJardin
+ */
+class TorreonJardin extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre             = 'torreon-jardin';
+        $this->autor              = 'Arq. Cecilio Pedro Secunza Schott';
+        $this->fecha              = '2018-07-03T10:15';
+        // El nombre del archivo a crear y rutas relativas a las imágenes
+        $this->archivo            = 'torreon-jardin';
+        $this->imagen             = 'torreon-jardin/imagen.jpg';
+        $this->imagen_previa      = 'torreon-jardin/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion        = 'Ubicación y límites de la Colonia Torreón Jardín';
+        $this->claves             = 'colonias';
+        // El directorio en la raíz donde se guardará el archivo HTML
+        $this->directorio         = 'sig-mapas-torreon';
+        // Opción del menú Navegación a poner como activa cuando vea esta publicación
+        $this->nombre_menu        = 'Información Geográfica';
+        // El estado puede ser publicar, revisar o ignorar
+        $this->estado             = 'Revisar';
+        // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
+        $this->para_compartir     = TRUE;
+        // Para el Organizador
+        $this->categorias         = array('Infraestructura');
+        // Para el botón de ver a pantalla completa
+        $this->url                = 'https://goo.gl/maps/4zdoQEoFe942';
+        $this->url_etiqueta       = 'Ver a pantalla completa';
+        // Instancia de SchemaPostalAddress que tiene la localidad, municipio y país
+        $region                   = new \Base\SchemaPostalAddress();
+        $region->addressCountry   = 'MX';
+        $region->addressRegion    = 'Coahuila de Zaragoza';
+        $region->addressLocality  = 'Torreón';
+        // Instancia de SchemaMapa con el mapa en CartoDB
+        $mapa                     = new \Base\SchemaMap();
+        $mapa->mapType            = 'VenueMap';
+        $mapa->url                = $this->url;
+        $mapa->url_label          = $this->url_etiqueta;
+        $mapa->theMap             = <<<FINAL
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7200.6382711135775!2d-103.42896732333027!3d25.527744416443838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fdb92f84e07f7%3A0xe02df20db4c24cee!2zVG9ycmXDs24gSmFyZMOtbiwgVG9ycmXDs24sIENvYWgu!5e0!3m2!1ses-419!2smx!4v1530716515827" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+FINAL;
+        // Instancia de SchemaPlace agrupa la región y el mapa
+        $lugar                    = new \Base\SchemaPlace();
+        $lugar->address           = $region;
+        $lugar->hasMap            = $mapa;
+        // Instancia de SchemaCreativeWork lo empaca con más datos
+        $paquete                  = new \Base\SchemaCreativeWork();
+        $paquete->big_heading     = true;
+        $paquete->name            = $this->nombre;
+        $paquete->description     = $this->descripcion;
+        $paquete->author          = $this->autor;
+        $paquete->datePublished   = $this->fecha;
+        $paquete->headline_style  = $this->encabezado_color;
+        $paquete->image           = $this->imagen;
+        $paquete->contentLocation = $lugar;
+        // El contenido es una instancia de SchemaCreativeWork
+        $this->contenido          = $paquete;
+    } // constructor
+
+    /**
+     * Javascript
+     *
+     * @return string No hay código Javascript, entrega un texto vacío
+     */
+    public function javascript() {
+        // JavaScript
+        // Sin caja JS
+        // Ejecutar este método en el padre
+        return parent::javascript();
+    } // javascript
+
+    /**
+     * Redifusion HTML
+     *
+     * @return string Código HTML
+     */
+    public function redifusion_html() {
+        // Para redifusión, si tiene una imagen, se pone la imagen y después el contenido
+        if ($this->imagen != '') {
+            $this->redifusion = sprintf("<a href=\"%s\"><img src=\"%s\"><br>\n\n%s</a>", "{$this->archivo}.html", $this->imagen, $this->descripcion);
+        } else {
+            $this->redifusion = sprintf('<a href="%s">%s</a>', "{$this->archivo}.html", $this->descripcion);
+        }
+        // Ejecutar este método en el padre
+        return parent::redifusion_html();
+    } // redifusion_html
+
+} // Clase TorreonJardin
+
+?>
