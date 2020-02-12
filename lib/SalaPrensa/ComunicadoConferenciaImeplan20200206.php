39491eecb7230fdeb853de3eb1b09fe69cc715c7
Victor Gomez <vmparadigma@hotmail.com>
2020-02-12 13:26:04 -0600

    Subí Comunicado de Prensa, postura ante imeplan

--- lib/SalaPrensa/ComunicadoConferenciaImeplan20200206.php
+++ lib/SalaPrensa/ComunicadoConferenciaImeplan20200206.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * TrcIMPLAN Sitio Web - ComunicadoPrensa20200130PrimeraSesionConsejo
+ * TrcIMPLAN Sitio Web - ComunicadoConferenciaImeplan20200206
  *
  * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
  *
@@ -23,9 +23,9 @@
 namespace SalaPrensa;
 
 /**
- * Clase ComunicadoPrensa20200130PrimeraSesionConsejo
+ * Clase ComunicadoConferenciaImeplan20200206
  */
-class ComunicadoPrensa20200130PrimeraSesionConsejo extends \Base\PublicacionSchemaBlogPosting {
+class ComunicadoConferenciaImeplan20200206 extends \Base\PublicacionSchemaBlogPosting {
 
     /**
      * Constructor
@@ -34,22 +34,22 @@ class ComunicadoPrensa20200130PrimeraSesionConsejo extends \Base\PublicacionSche
         // Ejecutar constructor en el padre
         parent::__construct();
         // Título, autor y fecha
-        $this->nombre                     = 'Primer Sesión de Consejo 2020. Se renueva el Consejo Directivo del IMPLAN.';
+        $this->nombre                     = 'Postura sobre la creación de un Instituto Metropolitano de Planeación.';
         $this->autor                      = 'Lic. Maira Ivonne Flores Reyes';
-        $this->fecha                      = '2020-01-30T00:00';
+        $this->fecha                      = '2020-02-06T00:00';
         // El nombre del archivo a crear
-        $this->archivo                    = '2020-01-30-primer-sesion-consejo-2020';
+        $this->archivo                    = '2020-02-06-comunicado-conferencia-imeplan';
         // La descripción y claves dan información a los buscadores y redes sociales
-        $this->descripcion                = 'En la Primera Sesión del año del Consejo Directivo del Instituto Municipal de Planeación y Competitividad de Torreón (IMPLAN), se presentaron a los nuevos Consejeros que se integrarán al organismo.';
-        $this->claves                     = 'IMPLAN, Torreon, consejo';
+        $this->descripcion                = 'Postura sobre la creación de un Instituto Metropolitano de Planeación.';
+        $this->claves                     = 'IMPLAN, Torreon, imeplan';
         // Ruta al archivo markdown con el contenido
-        $this->contenido_archivo_markdown = 'lib/SalaPrensa/ComunicadoPrensa20200130PrimeraSesionConsejo.md';
+        $this->contenido_archivo_markdown = 'lib/SalaPrensa/ComunicadoConferenciaImeplan20200206.md';
         // Para el Organizador
         $this->categorias                 = array();
         $this->fuentes                    = array();
         $this->regiones                   = array('Torreón', 'Gómez Palacio', 'Lerdo', 'Matamoros', 'La Laguna');
     } // constructor
 
-} // Clase ComunicadoPrensa20200130PrimeraSesionConsejo
+} // Clase ComunicadoConferenciaImeplan20200206
 
 ?>
