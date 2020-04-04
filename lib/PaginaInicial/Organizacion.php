f80b16b469e0e9e38c96ccbdab22a04a8cdcaa88
Victor Gomez <vmparadigma@hotmail.com>
2020-04-03 18:53:44 -0600

    Subí banner de Programa Zona Norte2

--- lib/PaginaInicial/Organizacion.php
+++ lib/PaginaInicial/Organizacion.php
@@ -105,6 +105,7 @@ class Organizacion extends \Base\SchemaGovernmentOrganization {
                // $b[] = '<a href="http://www.trcimplan.gob.mx/vision-metropoli/index.html"><img src="imagenes\convocatorias\convocatorias-2018-vm-banner-formulario-04.jpg" alt="Visión Metrópoli"></a>';
                // $b[] = '<a href="http://www.trcimplan.gob.mx/convocatorias/consejeros.html"><img src="imagenes\convocatorias\convocatorias-2018-cs-banner-09.jpg" alt="Consejeros ciudadanos"></a>';
                // $b[] = '<a href="http://www.trcimplan.gob.mx/convocatorias/consejeros.html"><img src="imagenes\convocatorias\convocatoria-consejeros-sectoriales-2019.jpg" alt="Consejeros ciudadanos 2019"></a>';
+                $b[] = '<a href="http://bit.ly/EncuestaPZN"><img src="imagenes\programa-zona-norte\banner-zona-norte.jpg" alt="Programa Zona Norte"></a>';
                 $b[] = '<a href="http://www.trcimplan.gob.mx/programas-radio/index.html"><img src="imagenes\radio\publicidad-radio3.jpg" alt="Programa de Radio Implan"></a>';
                // $b[] = '<a href="http://www.trcimplan.gob.mx/convocatorias/identidad-lagunera.html"><img src="imagenes\convocatorias\identidad-lagunera-2019.jpg" alt="Identidad Lagunera"></a>';
 
