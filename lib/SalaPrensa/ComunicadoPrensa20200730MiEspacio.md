864833e57a740f6471c1220146ca1c7b17d645f4
Victor Gomez <vmparadigma@hotmail.com>
2020-07-30 09:49:49 -0500

    Subí artículo de prensa: Mi Espacio, se subió también a prensa Visión Metrópoli

---
+++ lib/SalaPrensa/ComunicadoPrensa20200730MiEspacio.md
@@ -0,0 +1,27 @@
+
+
+<img src="2020-07-20-mi-espacio/ima01.jpg" alt="imagen mi espacio">
+
+##Recopilación de proyectos y recomendaciones para enfrentar al COVID-19</h1>
+
+</br></br>
+En diciembre del 2019 se detectó un nuevo virus denominado COVID-19, el cual se extendió por todo el mundo y la Organización Mundial de la Salud (OMS) declaró una pandemia global en rueda de prensa el 11 de marzo del 2020.
+El 30 de marzo del 2020, se declaró emergencia sanitaria a nivel nacional en México, suspendiendo las actividades no esenciales de los sectores públicos y privados.
+</br></br>
+Bajo los términos Home-Office, Home-School y #QuédateEnCasa estudiantes y trabajadores han debido adaptarse a un modelo de actividades primordialmente en línea, para no suspender por completo sus procesos. Las medidas preventivas en las actividades cotidianas continuarán vigentes por el resto del 2020.
+</br></br>
+Este documento ha sido creado con la finalidad de hacer más ameno el periodo de contingencia sanitaria y facilitar las condiciones de trabajo y/o estudio en casa, reconociendo el nuevo esquema que nos obliga a repensar la cotidianidad, así como del uso de los espacios y de los mobiliarios que hay en el hogar, no necesariamente adaptados a actividades con fines laborales y/o académicos.
+
+Los proyectos y acciones que se presentan, son resultantes de diversos esfuerzos del Consejo Visión Metrópoli en conjunto con el IMPLAN Torreón, impulsados hacia la ciudadanía.
+</br></br>
+Aquí, se otorgan opciones a los ciudadanos de adaptar espacios del hogar con muebles autoconstruibles de fácil armado y bajo costo.
+</br></br>
+También, se brindan alternativas de actividades que se pueden realizar en casa, clasificándolas en cuatro categorías; culturales, educativas, de entretenimiento y estilo de vida (lifestyle).
+</br></br>
+Se espera que quien lea el documento, pueda obtener diversas estrategias, tanto físicas, como digitales, para hacer el periodo de contingencia mucho más amable y llevadero.
+</br></br>
+Deseando que el documento sea de tu utilidad y agrado, ¡compártelo y disfrútalo!.
+</br></br></br>
+<a href="http://www.trcimplan.gob.mx/vision-metropoli/mi-espacio/mi-espacio.pdf"><b>HAZ CLICK PARA DESCARGAR EL BOOKLET “MI ESPACIO” EN PDF (14.5 Mb)</b></a>
+
+</br></br></br>
---
+++ lib/SalaPrensa/ComunicadoPrensa20200730MiEspacio.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - ComunicadoPrensa20200730MiEspacio
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace SalaPrensa;
+
+/**
+ * Clase ComunicadoPrensa20200730MiEspacio
+ */
+class ComunicadoPrensa20200730MiEspacio extends \Base\PublicacionSchemaBlogPosting {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Ejecutar constructor en el padre
+        parent::__construct();
+        // Título, autor y fecha
+        $this->nombre                     = 'Mi Espacio: Recopilación de proyectos y recomendaciones para enfrentar al COVID-19';
+        $this->autor                      = 'Arq. Alejandra Martínez Avilés';
+        $this->fecha                      = '2020-07-30T00:00';
+        // El nombre del archivo a crear
+        $this->archivo                    = '2020-07-20-mi-espacio';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion                = 'En diciembre del 2019 se detectó un nuevo virus denominado COVID-19, el cual se extendió por todo el mundo y la Organización Mundial de la Salud (OMS) declaró una pandemia global en rueda de prensa el 11 de marzo del 2020.
+El 30 de marzo del 2020, se declaró emergencia sanitaria a nivel nacional en México, suspendiendo las actividades no esenciales de los sectores públicos y privados.';
+        $this->claves                     = 'IMPLAN, Torreon, covid, sanitaria';
+        // Ruta al archivo markdown con el contenido
+        $this->contenido_archivo_markdown = 'lib/SalaPrensa/ComunicadoPrensa20200730MiEspacio.md';
+        // Para el Organizador
+        $this->categorias                 = array();
+        $this->fuentes                    = array();
+        $this->regiones                   = array('Torreón', 'Gómez Palacio', 'Lerdo', 'Matamoros', 'La Laguna');
+    } // constructor
+
+} // Clase ComunicadoPrensa20200730MiEspacio
+
+?>
