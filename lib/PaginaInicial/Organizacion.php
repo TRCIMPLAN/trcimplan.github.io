7f0903a8224f20b4337beb09c7cebfe5933c110a
Victor Gomez <vmparadigma@hotmail.com>
2020-08-28 09:32:17 -0500

    Cambio en Menú banners

--- lib/PaginaInicial/Organizacion.php
+++ lib/PaginaInicial/Organizacion.php
@@ -106,7 +106,8 @@ class Organizacion extends \Base\SchemaGovernmentOrganization {
                 $b[] = '<a href="https://solucionessig.com.mx/ENCUESTA_GENERAL/Index.html"><img src="imagenes\convocatorias\convocatoria-pddu-11.jpg" alt="Convocatoria PDDU"></a>';
                 $b[] = '<a href="http://www.trcimplan.gob.mx/convocatorias/manos-a-la-cebra2.html"><img src="imagenes\convocatorias\convocatoria-manos-a-la-cebra-banner2020.jpg" alt="Concurso Manos a La Cebra 2020"></a>';
                 $b[] = '<a href="http://www.trcimplan.gob.mx/vision-metropoli/mi-espacio.html"><img src="imagenes\mi-espacio\banner-mi-espacio.jpg" alt="Programa Mi Espacio"></a>';
-                $b[] = '<a href="https://bit.ly/ConsultaCentroHistorico"><img src="imagenes\centro-historico\pch-baner-19.jpg" alt="Centro Histórico"></a>';
+                $b[] = '<a href="https://solucionessig.com.mx/ENCUESTA_GENERAL/Index.html"><img src="imagenes\convocatorias\convocatoria-pddu-11.jpg" alt="Convocatoria PDDU"></a>';
+                // PONER OTRA VEZ $b[] = '<a href="https://bit.ly/ConsultaCentroHistorico"><img src="imagenes\centro-historico\pch-baner-19.jpg" alt="Centro Histórico"></a>';
                 $b[] = '<a href="http://www.trcimplan.gob.mx/convocatorias/pzn-2020.html"><img src="imagenes\programa-zona-norte\pzn-banner-general.jpg" alt="Programa Zona Norte"></a>';
                 $b[] = '<a href="http://www.trcimplan.gob.mx/investigaciones/politica-industrial.html"><img src="imagenes\politica-industrial\banner-politica-industrial2019.jpg" alt="Política Industrial 2019"></a>';
                 // PONER OTRA VEZ $b[] = '<a href="http://www.trcimplan.gob.mx/convocatorias/vision-metropoli.html"><img src="imagenes\convocatorias\convocatorias-mar2020-vm-banner.jpg" alt="Convocatorias VM 2020"></a>';
