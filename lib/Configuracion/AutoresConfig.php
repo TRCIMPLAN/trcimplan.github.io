e22d836e2ea163380a5a48bef7fdfef94b69cd13
Victor Gomez <vmparadigma@hotmail.com>
2020-10-16 15:00:44 -0500

    Subí foto de Daniela Sánchez

--- lib/Configuracion/AutoresConfig.php
+++ lib/Configuracion/AutoresConfig.php
@@ -75,6 +75,7 @@ class AutoresConfig {
         $this->autores[] = new \Base\Autor('', 'C.P.', 'Janeth Ramírez Facio',              'cp-janeth-ramirez-facio',               'IMPLAN Torreón', 'Auxiliar Administrativo',               'Tel. de Contacto: (871) 5007078', 'info@trcimplan.gob.mx', 'JanethFacio');
         $this->autores[] = new \Base\Autor('', 'Lic.', 'Edgardo de la Cruz Torres',         'lic-edgardo-de-la-cruz-torres',         'IMPLAN Torreón', 'Instrumentación política y jurídica',   'Tel. de Contacto: (871) 5007078', 'info@trcimplan.gob.mx', '');
 
+        $this->autores[] = new \Base\Autor('', 'Arq.', 'Olga Daniela Sánchez Valles',       'arq-olga-daniela-sanchez-valles',       'IMPLAN Torreón', 'Regeneración Urbana',                   'Tel. de Contacto: (871) 5007078, ext.2316', 'info@trcimplan.gob.mx', '');
 
     //~ $this->autores[] = new \Base\Autor('', 'Lic.', 'Javier de Jesús Valencia Jiménez',  'lic-javier-de-jesus-valencia-jimenez',  'IMPLAN Torreón', 'Auxiliar Administrativo',               '', 'javier_111192@hotmail.com', '');
     //~ $this->autores[] = new \Base\Autor('', 'Ing.', 'Luis Campos Hinojosa',              'unknown',                               'IMPLAN Torreón', 'Director de Investigación Estratégica', '', 'lcampos@trcimplan.gob.mx', '');
