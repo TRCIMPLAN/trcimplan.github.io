3f7f570d32f8664a74896ce9772ea3539956b222
Victor Gomez <vmparadigma@hotmail.com>
2021-03-18 10:24:12 -0600

    Cambios en Menú de sitio web, en Visión Metrópoli, redireccionamiento de opción a otro link

--- lib/Configuracion/NavegacionConfig.php
+++ lib/Configuracion/NavegacionConfig.php
@@ -101,7 +101,7 @@ class NavegacionConfig {
             'Consejo Visión Metrópoli > Integrantes del Consejo'      => 'vision-metropoli/integrantes.html',
             'Consejo Visión Metrópoli > Sala de Prensa'               => 'vision-metropoli/index.html',
             'Consejo Visión Metrópoli > Reglamento'                   => 'vision-metropoli/reglamento-vision-metropoli-2020.pdf',
-            'Consejo Visión Metrópoli > Informe 2019 - 2021'          => 'https://drive.google.com/file/d/1MCanhtN67NtL6xKbrOZ7uGoqegwMRD8O/view?usp=sharing'),
+            'Consejo Visión Metrópoli > Informe 2019 - 2021'          => 'https://issuu.com/consejovisionmetropoli/docs/informe_visi_n_metr_poli_2019_-_2021'),
       //  'Visión Metrópoli'                                            => 'vision-metropoli/index.html',
         'Institucional' => array(
             'Institucional > Visión / Misión'                         => 'institucional/vision-mision.html',
