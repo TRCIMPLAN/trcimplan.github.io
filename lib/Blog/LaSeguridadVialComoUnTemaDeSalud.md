5a0aaf3a47cc702a50c4e3a3623c9c764a1dae57
Victor Gomez <vmparadigma@hotmail.com>
2021-04-09 13:05:09 -0500

    Programé módulo de Obras 2021 en información geográfica/mapas/tema infraestructura

---
+++ lib/Blog/LaSeguridadVialComoUnTemaDeSalud.md
@@ -0,0 +1,20 @@
+
+Los primeros casos de traumatismos ocasionados por un incidente vial se registraron en 1896, pero fue hasta 2004 cuando la Organización Mundial de la Salud (OMS), declaró que se trataba de una pandemia que estaba cobrando a nivel mundial alrededor de 1,35 millones de vidas y si lo comparamos con los 1,8 millones de personas que fallecieron por COVID 19 en el año 2020, podríamos incluso suponer que los incidentes viales son otro tipo de pandemia.
+
+Pero en este caso, las secuelas incluirían a 40 millones de personas lesionadas a causa de los choques con vehículo automotor. Es por esto que hoy en día los traumatismos ocasionados por estos eventos, como uno de los principales problemas de salud a nivel mundial.
+
+Aunado a esto, los incidentes viales también representan un impacto en la economía de las ciudades, ya que se deben cubrir los daños materiales ocasionados, en promedio, una persona gasta alrededor de 25 mil pesos por incidente, si algún involucrado termina con un tipo de discapacidad.
+
+La OMS reconoció en 2015, que los traumatismos generados por incidentes de tránsito era una de las principales causas de muerte en personas de 15 a 29 años, también se revela que el 46% por muertes en estos eventos, corresponden a peatones que circulaban por la vía pública. El daño ocasionado a la víctima del incidente es proporcional a la velocidad a la que circula el vehículo, es decir mientras mayor sea esta, más probabilidades tiene una persona de resultar gravemente lesionada o fallecer durante o después del incidente.
+
+
+
+En el año 2019 en Torreón, se registraron 6,517 incidentes viales, de estos 346 fueron atropellos, esta cantidad disminuyó a 5,527 en 2020, sin embargo es muy importante tomar en cuenta que, la situación generada por la pandemia redujo la movilidad en la ciudad, debido al confinamiento ante la emergencia sanitaria, si esto no hubiera pasado probablemente la cantidad de incidentes sería mayor que la del año anterior.
+
+En términos de planeación urbana, las condiciones del contexto en el que ocurre el accidente son muy importantes, es importante tener tres enfoques:
+
+**1)** El preventivo, ya que se debe contar con un diseño urbano que evite el riesgo a que ocurra un incidente ofreciendo: banquetas suficientemente anchas, pasos peatonales (de cebra) con correcta semaforización, rampas para discapacitados, ciclovías; en el caso del transporte público los carriles exclusivos, y finalmente para automóviles los carriles correctamente definidos y con menores dimensiones, entre otras medidas para reducir su velocidad, señalética clara y semaforización sincronizada.
+
+**2)** Durante el incidente se debe garantizar la seguridad de todos aquellos que se hayan visto involucrados; con objetos protectores contra choques en las aceras y servicios de socorro cercanos.
+
+**3)** Después del incidente, se deben conocer los principales puntos de acceso para ambulancias y autoridades correspondientes.
---
+++ lib/SIGMapasTorreon/Obras2021.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * TrcIMPLAN Sitio Web - SIGMapasTorreon Obras2021
+ *
+ * Copyright (C) 2017 Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package TrcIMPLANSitioWeb
+ */
+
+namespace SIGMapasTorreon;
+
+/**
+ * Clase Obras2021
+ */
+class Obras2021 extends \Base\Publicacion {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        // Título, autor y fecha
+        $this->nombre             = 'Obras 2021';
+        $this->autor              = 'Dirección de Proyectos Estratégicos, Implan';
+        $this->fecha              = '2021-04-09T12:44';
+        // El nombre del archivo a crear y rutas relativas a las imágenes
+        $this->archivo            = 'obras-2021';
+        $this->imagen             = 'obras-2021/imagen.jpg';
+        $this->imagen_previa      = 'obras-2021/imagen-previa.jpg';
+        // La descripción y claves dan información a los buscadores y redes sociales
+        $this->descripcion        = 'Georreferenciación de proyectos de Obra pública realizados en 2021 en el municipio de Torreón, Coahuila.';
+        $this->claves             = 'obras, infraestructura';
+        // El directorio en la raíz donde se guardará el archivo HTML
+        $this->directorio         = 'sig-mapas-torreon';
+        // Opción del menú Navegación a poner como activa cuando vea esta publicación
+        $this->nombre_menu        = 'Información Geográfica';
+        // El estado puede ser publicar, revisar o ignorar
+        $this->estado             = 'Publicar';
+        // Si para compartir es verdadero, aparecerán al final los botones de compartir en Twitter y Facebook
+        $this->para_compartir     = TRUE;
+        // Para el Organizador
+        $this->categorias         = array('Infraestructura');
+        // Para el botón de ver a pantalla completa
+        // Sin URL
+        $this->url_etiqueta       = 'Ver a pantalla completa';
+        // Instancia de SchemaPostalAddress que tiene la localidad, municipio y país
+        $region                   = new \Base\SchemaPostalAddress();
+        $region->addressCountry   = 'MX';
+        $region->addressRegion    = 'Coahuila de Zaragoza';
+        $region->addressLocality  = 'Torreón';
+        // Instancia de SchemaMapa con el mapa en CartoDB
+        $mapa                     = new \Base\SchemaMap();
+        $mapa->mapType            = 'VenueMap';
+        $mapa->url                = $this->url;
+        $mapa->url_label          = $this->url_etiqueta;
+        $mapa->theMap             = <<<FINAL
+</br> <b>Fuente: Dirección de Proyectos Estratégicos Implan, con información de Transparencia Torreón. http://www.torreon.gob.mx/transparencia/info_o.cfm </b>
+</br><b>VERSIÓN DE DIVULGACIÓN E INFORMACIÓN, NO PRODUCE EFECTOS JURÍDICOS.</b>
+</br></br></br>
+FINAL;
+        // Instancia de SchemaPlace agrupa la región y el mapa
+        $lugar                    = new \Base\SchemaPlace();
+        $lugar->address           = $region;
+        $lugar->hasMap            = $mapa;
+        // Instancia de SchemaCreativeWork lo empaca con más datos
+        $paquete                  = new \Base\SchemaCreativeWork();
+        $paquete->big_heading     = true;
+        $paquete->name            = $this->nombre;
+        $paquete->description     = $this->descripcion;
+        $paquete->author          = $this->autor;
+        $paquete->datePublished   = $this->fecha;
+        $paquete->headline_style  = $this->encabezado_color;
+        $paquete->image           = $this->imagen;
+        $paquete->contentLocation = $lugar;
+        // El contenido es una instancia de SchemaCreativeWork
+        $this->contenido          = $paquete;
+    } // constructor
+
+    /**
+     * Javascript
+     *
+     * @return string No hay código Javascript, entrega un texto vacío
+     */
+    public function javascript() {
+        // JavaScript
+        // Sin caja JS
+        // Ejecutar este método en el padre
+        return parent::javascript();
+    } // javascript
+
+    /**
+     * Redifusion HTML
+     *
+     * @return string Código HTML
+     */
+    public function redifusion_html() {
+        // Para redifusión, si tiene una imagen, se pone la imagen y después el contenido
+        if ($this->imagen != '') {
+            $this->redifusion = sprintf("<a href=\"%s\"><img src=\"%s\"><br>\n\n%s</a>", "{$this->archivo}.html", $this->imagen, $this->descripcion);
+        } else {
+            $this->redifusion = sprintf('<a href="%s">%s</a>', "{$this->archivo}.html", $this->descripcion);
+        }
+        // Ejecutar este método en el padre
+        return parent::redifusion_html();
+    } // redifusion_html
+
+} // Clase Obras2021
+
+?>
