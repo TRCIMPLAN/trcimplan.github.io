e10fed45991afdfb98c4659071b576f797716081
Victor Gomez <vmparadigma@hotmail.com>
2022-02-02 14:55:53 -0600

    Hice cambios en reglamento de Visión metrópoli

--- lib/Configuracion/NavegacionConfig.php
+++ lib/Configuracion/NavegacionConfig.php
@@ -105,7 +105,7 @@ class NavegacionConfig {
             'Consejo Visión Metrópoli > Integrantes del Consejo'      => 'vision-metropoli/integrantes.html',
             'Consejo Visión Metrópoli > Sala de Prensa'               => 'vision-metropoli/index.html',
             'Consejo Visión Metrópoli > Publicaciones'                => 'vision-metropoli/publicaciones.html',
-            'Consejo Visión Metrópoli > Reglamento'                   => 'vision-metropoli/reglamento-vision-metropoli-2020.pdf'),
+            'Consejo Visión Metrópoli > Reglamento'                   => 'vision-metropoli/reglamento-vision-metropoli-2022.pdf'),
 //            'Consejo Visión Metrópoli > Informe 2019 - 2021'          => 'https://issuu.com/consejovisionmetropoli/docs/informe_visi_n_metr_poli_2019_-_2021'),
 
         'Institucional' => array(
